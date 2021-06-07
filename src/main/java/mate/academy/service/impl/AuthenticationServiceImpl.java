d5d71898602028686c684f19313f1772b7ec2680
safordog <safordog@gmail.com>
2021-06-07 19:15:55 +0300

    added solution

---
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,36 @@
+package mate.academy.service.impl;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> userByEmailOptional = userService.findByEmail(email);
+        if (userByEmailOptional.isPresent()
+                && userByEmailOptional.get().getPassword()
+                .equals(HashUtil.hashPassword(password, userByEmailOptional.get().getSalt()))) {
+            return userByEmailOptional.get();
+        }
+        throw new AuthenticationException("Can't authenticate user");
+    }
+
+    @Override
+    public User register(String email, String password) throws AuthenticationException {
+        if (userService.findByEmail(email).isEmpty()) {
+            return userService.add(new User(email, password));
+        }
+        throw new AuthenticationException("User with such email: " + email
+        + " has already been inserted to the DB");
+    }
+}
