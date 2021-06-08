819a11755bfc06060856cdca927da233bb88baef
Mariia Chekmak <mariiachekmak@MacBook-Pro-Mariia.local>
2021-06-08 20:53:21 +0300

    completed task

---
+++ src/main/java/mate/academy/dao/impl/UserDaoImpl.java
@@ -0,0 +1,49 @@
+package mate.academy.dao.impl;
+
+import java.util.Optional;
+import mate.academy.dao.UserDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.User;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+@Dao
+public class UserDaoImpl implements UserDao {
+    private static final SessionFactory factory = HibernateUtil.getSessionFactory();
+
+    @Override
+    public User add(User user) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+            return user;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert user entity to DB" + user, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        try (Session session = factory.openSession()) {
+            return session.createQuery("from User u where u.email = :mail", User.class)
+                    .setParameter("mail", email)
+                    .uniqueResultOptional();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't find user by email: " + email, e);
+        }
+    }
+}
---
+++ src/main/java/mate/academy/security/AuthenticationServiceImpl.java
@@ -0,0 +1,40 @@
+package mate.academy.security;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> optionalUser = userService.findByEmail(email);
+        User user = null;
+        if (optionalUser.isPresent()) {
+            user = optionalUser.get();
+            if (user.getPassword().equals(HashUtil.hash(password, user.getSalt()))) {
+                return user;
+            }
+        }
+        throw new AuthenticationException("Can't authenticate user by email " + email);
+    }
+
+    @Override
+    public User register(String email, String password) throws AuthenticationException {
+        User user = new User();
+        if (userService.findByEmail(email).isEmpty()) {
+            user.setEmail(email);
+            user.setPassword(password);
+            return userService.add(user);
+        }
+        throw new AuthenticationException("Can't register a new user. Such email " + email
+                    + " is already exists. Please enter another email!");
+    }
+}
