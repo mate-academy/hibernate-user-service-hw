4acbbbab1e50b1ced281c8cb47f8d3b95f85aef0
Никита Фоминых <nikitafominyh@MacBook-Pro-Nikita.local>
2021-06-08 17:58:01 +0300

    accepted task

---
+++ src/main/java/mate/academy/exception/AuthenticationException.java
@@ -0,0 +1,10 @@
+package mate.academy.exception;
+
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AuthenticationException(String message) {
+    }
+}
