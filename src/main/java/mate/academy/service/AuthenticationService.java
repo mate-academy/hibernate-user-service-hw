13a8434f91e2e07b2fc460d0cae7461aca82bc98
m.kislukhina <marharita.kislukhina@gmail.com>
2021-06-09 09:53:34 +0200

    done

---
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -0,0 +1,11 @@
+package mate.academy.service;
+
+import mate.academy.exception.AlreadyExistingEmailException;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.model.User;
+
+public interface AuthenticationService {
+    User login(String email, String password) throws AuthenticationException;
+
+    User register(String email, String password) throws AlreadyExistingEmailException;
+}
