cbcba89b678b3f2b7f1d91c7dbb8fe808a217abe
Vlad-Andr <andreev.v99@gmail.com>
2021-06-09 10:53:33 +0300

    fixed user check

--- src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -17,9 +17,12 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public User login(String email, String password) throws AuthenticationException {
         Optional<User> userFromDb = userService.findByEmail(email);
+        if (userFromDb.isEmpty()) {
+            throw new AuthenticationException("Can't authenticate user " + userFromDb);
+        }
         User user = userFromDb.get();
         String hashedPass = HashUtil.hashPassword(password, user.getSalt());
-        if (userFromDb.isPresent() && user.getPassword().equals(hashedPass)) {
+        if (user.getPassword().equals(hashedPass)) {
             return user;
         }
         throw new AuthenticationException("Can't authenticate user " + userFromDb);
