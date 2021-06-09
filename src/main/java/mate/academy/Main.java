2014acca51ccce31c9b495d611f91bc389412622
Andrii Romaniuk <romaniukandrii7@gmail.com>
2021-06-09 16:32:55 +0300

    first solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,55 +1,45 @@
 package mate.academy;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import mate.academy.model.CinemaHall;
-import mate.academy.model.Movie;
-import mate.academy.model.MovieSession;
-import mate.academy.service.CinemaHallService;
-import mate.academy.service.MovieService;
-import mate.academy.service.MovieSessionService;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.EmailAlreadyExistsException;
+import mate.academy.lib.Injector;
+import mate.academy.service.AuthenticationService;
 
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static AuthenticationService authenticationService
+            = (AuthenticationService) injector.getInstance(AuthenticationService.class);
+
     public static void main(String[] args) {
-        MovieService movieService = null;
-
-        Movie fastAndFurious = new Movie("Fast and Furious");
-        fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
-        movieService.add(fastAndFurious);
-        System.out.println(movieService.get(fastAndFurious.getId()));
-        movieService.getAll().forEach(System.out::println);
-
-        CinemaHall firstCinemaHall = new CinemaHall();
-        firstCinemaHall.setCapacity(100);
-        firstCinemaHall.setDescription("first hall with capacity 100");
-
-        CinemaHall secondCinemaHall = new CinemaHall();
-        secondCinemaHall.setCapacity(200);
-        secondCinemaHall.setDescription("second hall with capacity 200");
-
-        CinemaHallService cinemaHallService = null;
-        cinemaHallService.add(firstCinemaHall);
-        cinemaHallService.add(secondCinemaHall);
-
-        System.out.println(cinemaHallService.getAll());
-        System.out.println(cinemaHallService.get(firstCinemaHall.getId()));
-
-        MovieSession tomorrowMovieSession = new MovieSession();
-        tomorrowMovieSession.setCinemaHall(firstCinemaHall);
-        tomorrowMovieSession.setMovie(fastAndFurious);
-        tomorrowMovieSession.setShowTime(LocalDateTime.now().plusDays(1L));
-
-        MovieSession yesterdayMovieSession = new MovieSession();
-        yesterdayMovieSession.setCinemaHall(firstCinemaHall);
-        yesterdayMovieSession.setMovie(fastAndFurious);
-        yesterdayMovieSession.setShowTime(LocalDateTime.now().minusDays(1L));
-
-        MovieSessionService movieSessionService = null;
-        movieSessionService.add(tomorrowMovieSession);
-        movieSessionService.add(yesterdayMovieSession);
-
-        System.out.println(movieSessionService.get(yesterdayMovieSession.getId()));
-        System.out.println(movieSessionService.findAvailableSessions(
-                        fastAndFurious.getId(), LocalDate.now()));
+        try {
+            authenticationService.register("romaniukandrii7@gmail.com", "12345678");
+        } catch (EmailAlreadyExistsException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            authenticationService.register("bob@gmail.com", "9863413");
+        } catch (EmailAlreadyExistsException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            authenticationService.register("alica@gmail.com", "16543");
+        } catch (EmailAlreadyExistsException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            authenticationService.login("romaniukandrii7@gmail.com", "12345678");
+        } catch (AuthenticationException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            authenticationService.login("bob@gmail.com", "9863413");
+        } catch (AuthenticationException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            authenticationService.login("alica@gmail.com", "16543");
+        } catch (AuthenticationException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
