bafeb8c58f134ec7e0acdf2c07d99ba5056f36f7
Stanislav Aleksandrovych <astanislav2361@gmail.com>
2021-06-09 11:15:13 +0300

    Fixed mistakes.

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -4,7 +4,7 @@ import mate.academy.exception.AuthenticationException;
 import mate.academy.model.User;
 
 public interface AuthenticationService {
-    User register(String email, String password) throws AuthenticationException;
+    User register(String email, String password);
 
     User login(String email, String password) throws AuthenticationException;
 }
