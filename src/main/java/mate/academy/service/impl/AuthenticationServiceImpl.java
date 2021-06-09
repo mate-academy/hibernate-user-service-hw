d7da703841cdfee625b6648c151326b76190ab61
Vlad-Andr <andreev.v99@gmail.com>
2021-06-09 17:34:47 +0300

    f

--- src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -17,7 +17,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public User login(String email, String password) throws AuthenticationException {
         Optional<User> userFromDB = userService.findByEmail(email);
-        if (userFromDB.isPresent() && !userFromDB.get().getPassword()
+        if (userFromDB.isPresent() && userFromDB.get().getPassword()
                 .equals(HashUtil.hashPassword(password, userFromDB.get().getSalt()))) {
             return userFromDB.get();
         }
