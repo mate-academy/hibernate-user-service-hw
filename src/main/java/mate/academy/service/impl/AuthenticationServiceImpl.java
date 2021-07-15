434632d8dc0e29299d03b7e9b171967b2de8fa70
Максим <mbelmasg@gmail.com>
2021-07-15 16:14:10 +0300

    fixed Travis fail

--- src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -17,7 +17,8 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public User login(String email, String password) throws AuthenticationException {
         Optional<User> userFromDbOptional = userService.findByEmail(email);
-        if (userFromDbOptional.isPresent() && matchesPasswords(userFromDbOptional.get(), password)) {
+        if (userFromDbOptional.isPresent()
+                && matchesPasswords(userFromDbOptional.get(), password)) {
             return userFromDbOptional.get();
         }
         throw new AuthenticationException("Can't authenticate user");
