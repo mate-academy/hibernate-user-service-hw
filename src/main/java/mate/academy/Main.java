04ee0583fa3ce8969a716ce8d12486e972036b79
Максим <mbelmasg@gmail.com>
2021-07-15 12:56:15 +0300

    done

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -2,14 +2,20 @@ package mate.academy;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import mate.academy.lib.Injector;
 import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
 import mate.academy.model.MovieSession;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
 import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
+import mate.academy.service.UserService;
 
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
         MovieService movieService = null;
 
@@ -51,5 +57,14 @@ public class Main {
         System.out.println(movieSessionService.get(yesterdayMovieSession.getId()));
         System.out.println(movieSessionService.findAvailableSessions(
                         fastAndFurious.getId(), LocalDate.now()));
+        UserService userService = (UserService) injector
+                .getInstance(UserService.class);
+        AuthenticationService authenticationService
+                = (AuthenticationService) injector.getInstance(AuthenticationService.class);
+
+        User user = authenticationService
+                    .login("someUser@somePostservice.someDomain", "somePassword");
+        authenticationService.login("someUser@somePostservice.someDomain", "somePassword");
+
     }
 }
--- src/main/java/mate/academy/service/impl/UserServiceImpl.java
+++ src/main/java/mate/academy/service/impl/UserServiceImpl.java
@@ -1,12 +1,12 @@
 package mate.academy.service.impl;
 
 import java.util.Optional;
-
 import mate.academy.dao.UserDao;
 import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.User;
 import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -15,6 +15,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User add(User user) {
+        user.setSalt(HashUtil.getSalt());
+        user.setPassword(HashUtil.hashPassword(user.getPassword(), user.getSalt()));
         return userDao.add(user);
     }
 
