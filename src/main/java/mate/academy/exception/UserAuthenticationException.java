529272d621e3cf87d80b12cfa83ec2702cc730a5
Denis <deniskichan@gmail.com>
2021-08-05 17:41:49 +0300

    implemented User model, dao and service levels for it as well as AuthenticationService. tested.

---
+++ src/main/java/mate/academy/exception/UserAuthenticationException.java
@@ -0,0 +1,7 @@
+package mate.academy.exception;
+
+public class UserAuthenticationException extends Exception {
+    public UserAuthenticationException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,42 @@
+package mate.academy.service.impl;
+
+import java.util.Optional;
+import mate.academy.exception.UserAuthenticationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws UserAuthenticationException {
+        Optional<User> optionalUser = userService.findByEmail(email);
+        if (optionalUser.isPresent() && optionalUser.get().getPassword()
+                .equals(HashUtil.hashPassword(password, optionalUser.get().getSalt()))) {
+            return optionalUser.get();
+        }
+        throw new UserAuthenticationException("email or password are incorrect");
+    }
+
+    @Override
+    public User register(String email, String password) throws UserAuthenticationException {
+        if (email == null || password == null
+                || password.length() == 0 || email.length() == 0) {
+            throw new UserAuthenticationException("Incorrect email or password");
+        }
+        Optional<User> optionalUser = userService.findByEmail(email);
+        if (optionalUser.isPresent()) {
+            throw new UserAuthenticationException("There is already an email in DB:");
+        }
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        return userService.add(user);
+    }
+}
