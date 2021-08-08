1cf52f3479ad0584b12b043c2fef607f7559fb1b
Nikitozin <nikitozin91@gmail.com>
2021-08-08 19:09:32 +0300

    Move_exception

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -9,8 +9,12 @@ public class Main {
     private static final AuthenticationService authenticationService =
             (AuthenticationService) injector.getInstance(AuthenticationService.class);
 
-    public static void main(String[] args) throws AuthenticationException {
+    public static void main(String[] args) {
         authenticationService.register("12345@gmail.com", "12345");
-        authenticationService.login("12345@gmail.com", "12345");
+        try {
+            authenticationService.login("12345@gmail.com", "12345");
+        } catch (AuthenticationException e) {
+            throw new RuntimeException("Email or password is invalid");
+        }
     }
 }
