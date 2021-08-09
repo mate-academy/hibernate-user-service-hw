863deaf0e4d2ce81a4495b47b27cea3c6a695ea1
EugeneSinica <eugenesinica@gmail.com>
2021-08-09 14:13:58 +0300

    added @Column(unique = true) in User.email

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -1,11 +1,7 @@
 package mate.academy.model;
 
 import java.util.Arrays;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
@@ -13,6 +9,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true, length = 50)
     private String email;
     private String password;
     private byte[] salt;
