8d3e50e2223251fa09041643b965c75bc2effe2e
Nikitozin <nikitozin91@gmail.com>
2021-08-09 15:15:54 +0300

    Exceptions

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,6 +1,7 @@
 package mate.academy;
 
 import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
 import mate.academy.lib.Injector;
 import mate.academy.service.AuthenticationService;
 
@@ -9,12 +10,16 @@ public class Main {
     private static final AuthenticationService authenticationService =
             (AuthenticationService) injector.getInstance(AuthenticationService.class);
 
-    public static void main(String[] args) {
-        authenticationService.register("12345@gmail.com", "12345");
+    public static void main(String[] args) throws AuthenticationException, RegistrationException {
+        try {
+            authenticationService.register("12345@gmail.com", "12345");
+        } catch (RegistrationException e) {
+            throw new RegistrationException("Can't register a user");
+        }
         try {
             authenticationService.login("12345@gmail.com", "12345");
         } catch (AuthenticationException e) {
-            throw new RuntimeException("Email or password is invalid");
+            throw new AuthenticationException("Email or password is invalid");
         }
     }
 }
