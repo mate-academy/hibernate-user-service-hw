b4a37a03875969c2da16e61e7d83a212ae38e805
Kostia Dementiev <konstde00@gmail.com>
2021-10-13 10:30:59 +0300

    added solution

---
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,39 @@
+package mate.academy.service.impl;
+
+import java.util.Optional;
+import mate.academy.dao.UserDao;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserDao userDao;
+
+    @Override
+    public User login(String email, String password)
+            throws AuthenticationException {
+        Optional<User> userOptional = userDao.findByEmail(email);
+        if (userOptional.isEmpty() || !userOptional.get().getPassword().equals(password)) {
+            throw new AuthenticationException("Can't find user with such login and password");
+        }
+        return userOptional.get();
+    }
+
+    @Override
+    public User register(String email, String password)
+            throws RegistrationException {
+        if (userDao.findByEmail(email).isPresent()) {
+            throw new RegistrationException("Can't register user with email " + email
+                    + ": email was already registered");
+        }
+        User user = new User();
+        user.setLogin(email);
+        user.setPassword(password);
+        return userDao.add(user);
+    }
+}
