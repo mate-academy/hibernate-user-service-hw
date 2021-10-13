67bd6a1cec3b807061fea45ff266cb8f42cf1c71
lavrivskyi <lavrivskyi@gmail.com>
2021-10-13 12:28:55 +0300

    Removed redundant method getAll() from UserDao

--- src/main/java/mate/academy/dao/UserDao.java
+++ src/main/java/mate/academy/dao/UserDao.java
@@ -1,13 +1,10 @@
 package mate.academy.dao;
 
-import java.util.List;
 import java.util.Optional;
 import mate.academy.model.User;
 
 public interface UserDao {
     User add(User user);
-
-    List<User> getAll();
 
     Optional<User> findByEmail(String email);
 }
