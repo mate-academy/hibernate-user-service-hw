12c00809698a887b4adf6b719997a6a1da857c8c
Yuliya Senchenko <usencenko70@gmail.com>
2021-10-29 11:08:36 +0300

    User service project (fixed)

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -62,7 +62,6 @@ public class User {
     public String toString() {
         return "User{"
                 + "id=" + id
-                + ", password='" + password + '\''
                 + ", login='" + email + '\''
                 + '}';
     }
