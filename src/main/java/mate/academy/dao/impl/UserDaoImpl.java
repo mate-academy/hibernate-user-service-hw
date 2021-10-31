71b786091888912a6d6a4d4ff332ad2ddc6bbebd
Denys Stefanko <denys.stefanko@gmail.com>
2021-10-31 15:41:29 +0300

    added userDao and UserService

---
+++ src/main/java/mate/academy/dao/impl/UserDaoImpl.java
@@ -0,0 +1,40 @@
+package mate.academy.dao.impl;
+
+import java.util.Optional;
+import mate.academy.dao.UserDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.User;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@Dao
+public class UserDaoImpl implements UserDao {
+    @Override
+    public User add(User user) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't save user in DB " + user, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return user;
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return Optional.empty();
+    }
+}
---
+++ src/main/java/mate/academy/service/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package mate.academy.service.impl;
+
+import java.util.Optional;
+
+import mate.academy.dao.UserDao;
+import mate.academy.lib.Inject;
+import mate.academy.model.User;
+import mate.academy.service.UserService;
+
+public class UserServiceImpl implements UserService {
+    @Inject
+    UserDao userDao;
+    @Override
+    public User add(User user) {
+        return userDao.add(user);
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return Optional.empty();
+    }
+}
