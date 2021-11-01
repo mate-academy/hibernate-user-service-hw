091daea9b9cf6ef1a5648206d6b9a88f332cdbb0
Taras Oleksiuk <64101459+taras-oleksiuk@users.noreply.github.com>
2021-11-01 11:56:16 +0200

    The password is hidden from method toString(). The size of an array of bytes is put in the constant.

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -65,7 +65,6 @@ public class User {
         return "User{"
                 + "id=" + id
                 + ", email='" + email + '\''
-                + ", password='" + password + '\''
                 + ", salt=" + Arrays.toString(salt)
                 + '}';
     }
