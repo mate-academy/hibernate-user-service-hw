f1c6a0ae1de02436fcad9fde82302bacc4ef4660
Taras Oleksiuk <64101459+taras-oleksiuk@users.noreply.github.com>
2021-11-01 12:55:23 +0200

    The sensitive info is hidden (added as a commit).

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -65,7 +65,6 @@ public class User {
         return "User{"
                 + "id=" + id
                 + ", email='" + email + '\''
-                + ", salt=" + Arrays.toString(salt)
                 + '}';
     }
 }
