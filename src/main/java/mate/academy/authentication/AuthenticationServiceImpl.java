ec9aa342090609a08cb8467fd6f1a7633490c72e
Andrii Demchenko <avd998ne@gmail.com>
2021-11-01 12:16:27 +0200

    added all needed classes and interfaces

---
+++ src/main/java/mate/academy/authentication/AuthenticationServiceImpl.java
@@ -0,0 +1,18 @@
+package mate.academy.authentication;
+
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.model.User;
+
+public class AuthenticationServiceImpl implements AuthenticationService {
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        return null;
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        return null;
+    }
+}
