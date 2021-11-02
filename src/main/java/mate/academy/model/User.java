d0d56299505dcf3925a58f405540e07d9489e61b
Nazar Hetun <nazarhetun@gmail.com>
2021-11-02 18:18:59 +0200

    delete password from toString method

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -57,7 +57,6 @@ public class User {
     public String toString() {
         return "User{"
                 + "id=" + id
-                + ", password='" + password + '\''
                 + ", email='" + email + '\''
                 + '}';
     }
