6ca847ca466f65832b1ac03593640c7ab434cd38
nishchenkokateryna <kateryna.nishchenko@gmail.com>
2021-12-24 11:48:46 +0200

    Removed alias from query, after student review

--- src/main/java/mate/academy/dao/impl/UserDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/UserDaoImpl.java
@@ -37,7 +37,7 @@ public class UserDaoImpl implements UserDao {
     @Override
     public Optional<User> findByEmail(String email) {
         try (Session session = getSessionFactory().openSession()) {
-            return session.createQuery("FROM User u WHERE u.email = :email", User.class)
+            return session.createQuery("FROM User WHERE email = :email", User.class)
                     .setParameter("email", email)
                     .uniqueResultOptional();
         }
