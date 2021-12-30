c79e9522c7509edf186284a224644c312d4d7e62
Yurii Khomiak <ykhomiak@st.swps.edu.pl>
2021-12-30 10:27:34 +0100

    minor fix in authentication service

--- src/main/java/mate/academy/security/AuthenticationService.java
+++ src/main/java/mate/academy/security/AuthenticationService.java
@@ -5,7 +5,7 @@ import mate.academy.exception.RegistrationException;
 import mate.academy.model.User;
 
 public interface AuthenticationService {
-    User register(String password, String login) throws RegistrationException;
+    User register(String login, String password) throws RegistrationException;
 
     User login(String login, String password) throws AuthenticationException;
 }
