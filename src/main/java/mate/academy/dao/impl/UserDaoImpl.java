3344091c9fa62b8480cc9488b8b6afaa7d6efa44
Mykhailo Tereshchenkov <mtwib.re@gmail.com>
2021-12-30 12:26:53 +0200

    В ТЗ такого не было >:-(

--- src/main/java/mate/academy/dao/impl/UserDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/UserDaoImpl.java
@@ -3,11 +3,13 @@ package mate.academy.dao.impl;
 import java.util.Optional;
 import mate.academy.dao.UserDao;
 import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
 import mate.academy.model.User;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+@Dao
 public class UserDaoImpl implements UserDao {
     @Override
     public User add(User user) {
