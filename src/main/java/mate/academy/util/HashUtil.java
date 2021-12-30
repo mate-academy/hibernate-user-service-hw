0c015f6f05354a15d71680c2ffdac20965d2da95
farfardesu <savavlad003@gmail.com>
2021-12-30 08:35:53 +0200

    Registration and Authentication were implemented as well as User model. User DAO and Service layers were implemented.

---
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -0,0 +1,35 @@
+package mate.academy.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class HashUtil {
+    public static final String CRYPTO_ALGORITHM = "SHA-512";
+
+    public HashUtil() {
+    }
+
+    public static byte[] getSalt() {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[64];
+        random.nextBytes(salt);
+        return salt;
+    }
+
+    public static String hashPassword(String password, byte[] salt) {
+        StringBuilder hashedPassword = new StringBuilder();
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance(CRYPTO_ALGORITHM);
+            messageDigest.update(salt);
+            byte[] digest = messageDigest.digest(password.getBytes());
+            for (byte b: digest) {
+                hashedPassword.append(String.format("%02x", b));
+            }
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException("Couldn't create hash using "
+                    + CRYPTO_ALGORITHM + " algorithm.");
+        }
+        return hashedPassword.toString();
+    }
+}
