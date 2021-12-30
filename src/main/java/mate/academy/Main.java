c187c9f97ae8161bce95fb486307079733e703bc
Illia Lavryniuk <lavryniuk.dev@gmail.com>
2021-12-30 17:14:11 +0200

    mvn clean package success

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -6,7 +6,7 @@ import mate.academy.lib.Injector;
 import mate.academy.service.AuthenticationService;
 
 public class Main {
-    private static Injector injector = Injector.getInstance("mate.academy");
+    private static final Injector injector = Injector.getInstance("mate.academy");
 
     public static void main(String[] args) {
         AuthenticationService authenticationService =
--- src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -13,7 +13,7 @@ import mate.academy.util.HashUtil;
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
     @Inject
-    UserService userService;
+    private UserService userService;
 
     @Override
     public User login(String email, String password) throws AuthenticationException {
