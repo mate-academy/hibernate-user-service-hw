0a0296f8331648fea0fef19952bf169510aafa04
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-03-02 13:54:51 +0200

    Add encryption to UserServiceImpl

--- src/main/java/mate/academy/service/impl/UserServiceImpl.java
+++ src/main/java/mate/academy/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.User;
 import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
 
 import java.util.Optional;
 
@@ -15,6 +16,9 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User add(User user) {
+        user.setSalt(HashUtil.getSalt());
+        user.setPassword(HashUtil.hashPassword(user.getPassword(),
+                user.getSalt()));
         return userDao.add(user);
     }
 
