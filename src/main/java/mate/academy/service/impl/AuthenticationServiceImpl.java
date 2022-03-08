cdd04803e06a1caf034f8c757e846a00744690c7
Yaroslav Boiko <slkawnia@gmail.com>
2022-03-08 21:46:31 +0200

    Implementation of HW Hibernate - create User service

---
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,53 @@
+package mate.academy.service.impl;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> optionalUser = userService.findByEmail(email);
+
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            String hashPassword = HashUtil.hashPassword(password, user.getSalt());
+
+            if (user.getPassword().equals(hashPassword)) {
+                return user;
+            }
+        }
+
+        throw new AuthenticationException("Can't authenticate user. "
+                + "Check your email or password!");
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        Optional<User> optionalUser = userService.findByEmail(email);
+
+        if (optionalUser.isPresent()) {
+            throw new RegistrationException("User exists!");
+        }
+
+        if (password.isEmpty()) {
+            throw new RegistrationException("Password can't be empty!");
+        }
+
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+
+        return userService.add(user);
+    }
+}
