28114fce722e3f437d9d494638f416e0999806ad
sviat-h <svyatoslav.grinik@gmail.com>
2022-03-19 21:26:09 +0200

    exceptions were refactored

--- src/main/java/mate/academy/exception/AuthenticationException.java
+++ src/main/java/mate/academy/exception/AuthenticationException.java
@@ -1,6 +1,10 @@
 package mate.academy.exception;
 
 public class AuthenticationException extends Exception {
+    public AuthenticationException(String message) {
+        super(message);
+    }
+
     public AuthenticationException(String message, Throwable cause) {
         super(message, cause);
     }
