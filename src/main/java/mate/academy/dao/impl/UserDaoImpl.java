53d6201aa3c6920a7cb6686f971676c5b4b39896
Olexsii_Titov <drfrankinsheyn16@gmail.com>
2022-04-12 11:22:12 +0300

    fixed after student review

--- src/main/java/mate/academy/dao/impl/UserDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/UserDaoImpl.java
@@ -25,7 +25,7 @@ public class UserDaoImpl implements UserDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new DataProcessingException("Can't insert a user" + user, e);
+            throw new DataProcessingException("Can't insert a user: " + user, e);
         } finally {
             if (session != null) {
                 session.close();
