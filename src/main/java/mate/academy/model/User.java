a61d180ba1595a13363eb5703ff2043238d9ae77
Oleh Mytiai <mytiai.ov@icloud.com>
2022-04-12 16:23:41 +0300

    implemented UserDao, added annotation to User class

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -1,6 +1,16 @@
 package mate.academy.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String email;
     private String password;
