06e53babc8249a5f482347d0826f3776e7acf2fb
IgorZima90 <zimyshkaa@gmail.com>
2022-04-13 01:25:52 +0300

    was implemented authentication service and user dao layer

---
+++ src/main/java/mate/academy/security/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,44 @@
+package mate.academy.security.impl;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.security.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        if (userService.findByEmail(email).isPresent()) {
+            throw new RegistrationException("Can't register user with email " + email);
+        }
+        if (password.isEmpty()) {
+            throw new RegistrationException("Can't register user with empty password");
+        }
+        User user = new User();
+        user.setPassword(password);
+        user.setEmail(email);
+        return userService.add(user);
+    }
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> userFromDbOptional = userService.findByEmail(email);
+        if (userFromDbOptional.isEmpty()) {
+            throw new AuthenticationException("Can't authenticate user");
+        }
+        User user = userFromDbOptional.get();
+        if (HashUtil.hashPassword(password, user.getSalt()).equals(user.getPassword())) {
+            return user;
+        }
+        throw new AuthenticationException("Can't authenticate user");
+    }
+}
