feb4694e8f19c0d621fa9dfd6c4e00025cfd4909
Ostap Danyliv <danyliv.ostap@gmail.com>
2022-04-12 10:56:18 +0300

    fixes made

--- src/main/java/mate/academy/security/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/security/AuthenticationServiceImpl.java
@@ -11,6 +11,8 @@ import mate.academy.util.HashUtil;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
+    public static final String USER_EMAIL_OR_PASSWORD_IS_INCORRECT =
+            "User email or password is incorrect";
     @Inject
     private UserService userService;
 
@@ -18,21 +20,20 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     public User login(String email, String password) throws AuthenticationException {
         Optional<User> userFromDbOptional = userService.findByEmail(email);
         if (userFromDbOptional.isEmpty()) {
-            throw new AuthenticationException("User email or password is incorrect");
+            throw new AuthenticationException(USER_EMAIL_OR_PASSWORD_IS_INCORRECT);
         }
         User user = userFromDbOptional.get();
         String hashedPassword = HashUtil.hashPassword(password, user.getSalt());
         if (user.getPassword().equals(hashedPassword)) {
             return user;
         } else {
-            throw new AuthenticationException("User email or password is incorrect");
+            throw new AuthenticationException(USER_EMAIL_OR_PASSWORD_IS_INCORRECT);
         }
     }
 
     @Override
     public User register(String email, String password) throws RegistrationException {
-        Optional<User> userByEmail = userService.findByEmail(email);
-        if (userByEmail.isPresent()) {
+        if (userService.findByEmail(email).isPresent()) {
             throw new RegistrationException("User with this email is already registered");
         }
         if (password.isEmpty()) {
--- src/main/java/mate/academy/util/HashUtil.java
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -6,14 +6,14 @@ import java.security.SecureRandom;
 
 public class HashUtil {
     private static final String CRYPTO_ALGORITHM = "SHA-512";
+    private static final int SALT_ARRAY_SIZE = 16;
 
     private HashUtil() {
-
     }
 
     public static byte[] getSalt() {
         SecureRandom secureRandom = new SecureRandom();
-        byte[] salt = new byte[16];
+        byte[] salt = new byte[SALT_ARRAY_SIZE];
         secureRandom.nextBytes(salt);
         return salt;
     }
