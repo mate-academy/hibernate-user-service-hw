89d5bcf04ba97c651deb041c994ada484c5d5551
Vitalii Bozadzhy <VitaliiBozadzhy@icloud.com>
2022-05-08 21:23:18 +0200

    - added all for solution

---
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -0,0 +1,30 @@
+package mate.academy.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class HashUtil {
+    public static byte[] getSalt() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] salt = new byte[16];
+        secureRandom.nextBytes(salt);
+        return salt;
+    }
+
+    public static String hashPassword(String password, byte[] salt) {
+        StringBuilder hashPasswordString = new StringBuilder();
+        try {
+            MessageDigest messageDigest = MessageDigest
+                    .getInstance("SHA-512");
+            messageDigest.update(salt);
+            byte[] digest = messageDigest.digest(password.getBytes());
+            for (byte b : digest) {
+                hashPasswordString.append(String.format("%02x",b));
+            }
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException("Can't not create hash", e);
+        }
+        return hashPasswordString.toString();
+    }
+}
