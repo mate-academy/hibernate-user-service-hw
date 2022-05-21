6a8766d26908e60114cf39208fc7e787ddfb445a
oleg-new <9213195@gmail.com>
2022-05-21 16:26:49 +0300

    class created user interfaces UserDAO User Service AuthenticationService and classes that implement these interfaces added custom Exceptions RegistrationException AuthenticationException

---
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,40 @@
+package mate.academy.service.impl;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> existingUser = userService.findByEmail(email);
+        String hashIncomingPassword = HashUtil.hashPassword(password, existingUser.get().getSalt());
+        if (existingUser.isEmpty()
+                || !existingUser.get().getPassword().equals(hashIncomingPassword)) {
+            throw new AuthenticationException("Wrong email or password");
+        }
+        return existingUser.get();
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        if (userService.findByEmail(email).isPresent()) {
+            throw new RegistrationException("user with " + email
+                    + " has already been registered before");
+        }
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        return userService.add(user);
+    }
+}
