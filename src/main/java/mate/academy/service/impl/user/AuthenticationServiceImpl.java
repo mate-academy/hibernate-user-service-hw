f6e7fdf60617484766a191b2db4bdace8691e0c3
Mark Shapka <mark.shapka1@gmail.com>
2022-05-24 19:54:24 +0300

    task hibernate-user-service - created User class - implemented userDaoImpl, userServiceImpl, AuthenticationServiceImpl classes - added hibernate.cfg.xml - created RegistrationException and AuthenticationException

---
+++ src/main/java/mate/academy/service/impl/user/AuthenticationServiceImpl.java
@@ -0,0 +1,37 @@
+package mate.academy.service.impl.user;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> userFromDB = userService.findByEmail(email);
+        if (userFromDB.isEmpty()
+                || !HashUtil.hashPassword(password, userFromDB.get().getSalt())
+                .equals(userFromDB.get().getPassword())) {
+            throw new AuthenticationException("Login or password are incorrect");
+        }
+        return userFromDB.get();
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        if (!userService.findByEmail(email).isEmpty() || password.isEmpty()) {
+            throw new RegistrationException("User exists with the same email: " + email);
+        }
+        User user = new User(email, password);
+        return userService.add(user);
+    }
+}
