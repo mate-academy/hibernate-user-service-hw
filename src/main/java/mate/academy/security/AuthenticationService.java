4da1c262f449d9c15fa9c0512246dc3e9b34ab12
Angela1608 <89303267+Angela1608@users.noreply.github.com>
2022-05-29 21:50:38 +0300

    solved hibernate-user-service-hw

--- src/main/java/mate/academy/security/AuthenticationService.java
+++ src/main/java/mate/academy/security/AuthenticationService.java
@@ -8,5 +8,4 @@ public interface AuthenticationService {
     User login(String email, String password) throws AuthenticationException;
 
     User register(String email, String password) throws RegistrationException;
-
 }
