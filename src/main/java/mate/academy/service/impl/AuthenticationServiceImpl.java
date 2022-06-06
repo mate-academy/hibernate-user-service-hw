b27c2a86279e23bdf959aa8779611685ca6a4689
Viktoriia Ihnatiuk <victoriia.ihnatiuk@gmail.com>
2022-06-06 15:42:24 +0300

    hibernate user service homework solution has been added

---
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,47 @@
+package mate.academy.service.impl;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> userOptional = userService.findByEmail(email);
+        if (!userOptional.isEmpty()) {
+            User user = userOptional.get();
+            String hashedPassword = HashUtil.hashPassword(password, user.getSalt());
+            if (user.getPassword().equals(hashedPassword)) {
+                return user;
+            }
+        }
+
+        throw new AuthenticationException("Email or password was incorrect.");
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        Optional<User> userOptional = userService.findByEmail(email);
+        if (userOptional.isPresent()) {
+            throw new RegistrationException("User with this email "
+                    + email + " is already registered.");
+        }
+        User user = new User();
+        user.setEmail(email);
+        user.setSalt(HashUtil.getSalt());
+        String hashedPassword = HashUtil.hashPassword(password, user.getSalt());
+        user.setPassword(hashedPassword);
+        userService.add(user);
+        return user;
+    }
+}
