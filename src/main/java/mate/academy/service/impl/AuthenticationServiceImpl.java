085ffb0c74db7b5727fe81ba7aa618031d6814ab
Vladyslav Fomenko <with.god.victorius@gmail.com>
2022-06-06 15:23:18 +0300

    user-service Fomenko

---
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,44 @@
+package mate.academy.service.impl;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> userFromDbOptional = userService.findByEmail(email);
+        if (userFromDbOptional.isPresent() && checkPassword(password, userFromDbOptional.get())) {
+            return userFromDbOptional.get();
+        }
+        throw new AuthenticationException("Wrong email or password");
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        if (userService.findByEmail(email).isEmpty() && !password.isEmpty() && !email.isEmpty()) {
+            User user = new User();
+            user.setEmail(email);
+            user.setPassword(password);
+            return userService.add(user);
+        } else {
+            throw new RegistrationException("Please check your login or password!\n"
+                    + "Maybe you already registration on this site?");
+        }
+    }
+
+    private boolean checkPassword(String rawPassword, User userFromDb) {
+        String hashedPassword = HashUtil.hashPassword(rawPassword, userFromDb.getSalt());
+        return hashedPassword.equals(userFromDb.getPassword());
+    }
+}
