6cf321814e33c3448c5a4fbcf6165dd886ba4066
Viktoriia Ihnatiuk <victoriia.ihnatiuk@gmail.com>
2022-06-06 17:40:24 +0300

    solution has been fixed due to mentor's remarks

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -17,6 +17,7 @@ import mate.academy.service.UserService;
 
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) throws RegistrationException, AuthenticationException {
         MovieService movieService = (MovieService) injector
                 .getInstance(MovieService.class);
