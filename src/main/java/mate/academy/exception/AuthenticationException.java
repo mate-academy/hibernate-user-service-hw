dd843834a478060596014009c3d378618f25b42b
anton-karpov <antonkarpov547@gmail.com>
2022-07-07 13:29:33 +0200

    redundant constructors of custom exceptions removed

--- src/main/java/mate/academy/exception/AuthenticationException.java
+++ src/main/java/mate/academy/exception/AuthenticationException.java
@@ -1,10 +1,6 @@
 package mate.academy.exception;
 
 public class AuthenticationException extends Exception {
-    public AuthenticationException(String message, Throwable throwable) {
-        super(message, throwable);
-    }
-
     public AuthenticationException(String message) {
         super(message);
     }
--- src/main/java/mate/academy/exception/RegistrationException.java
+++ src/main/java/mate/academy/exception/RegistrationException.java
@@ -1,10 +1,6 @@
 package mate.academy.exception;
 
 public class RegistrationException extends Exception {
-    public RegistrationException(String message, Throwable throwable) {
-        super(message, throwable);
-    }
-
     public RegistrationException(String message) {
         super(message);
     }
