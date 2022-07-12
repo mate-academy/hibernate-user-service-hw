ecf05c7ca4582eede5b243068701ffe4939c18fa
Igor <ZIM20093@gmail.com>
2022-07-12 20:43:14 +0300

    refactored imports

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,18 +1,9 @@
 package mate.academy;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import mate.academy.exception.AuthenticationException;
 import mate.academy.exception.RegistrationException;
 import mate.academy.lib.Injector;
-import mate.academy.model.CinemaHall;
-import mate.academy.model.Movie;
-import mate.academy.model.MovieSession;
 import mate.academy.security.AuthenticationService;
-import mate.academy.service.CinemaHallService;
-import mate.academy.service.MovieService;
-import mate.academy.service.MovieSessionService;
-import mate.academy.service.UserService;
 
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.academy");
