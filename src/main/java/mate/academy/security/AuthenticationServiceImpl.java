e5c586edf40dbcd2db98f644764c8da39ee88d8e
Mykola Yakymiv <mykola8426@gmail.com>
2022-07-26 14:47:33 +0300

    model user, dao and service for it were added

---
+++ src/main/java/mate/academy/security/AuthenticationServiceImpl.java
@@ -0,0 +1,40 @@
+package mate.academy.security;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> user = userService.findByEmail(email);
+        if (user.isPresent() && HashUtil.hashPassword(password, user.get().getSalt())
+                .equals(user.get().getPassword())) {
+            return user.get();
+        }
+        throw new AuthenticationException("Login or password was incorrect");
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        Optional<User> user = userService.findByEmail(email);
+        if (user.isPresent()) {
+            throw new RegistrationException("User with same email already exist."
+                    + "Change email or try to login");
+        }
+        if (password.length() < 10) {
+            throw new RegistrationException("Password must contain 10 or more symbols");
+        }
+        User current = new User();
+        current.setEmail(email);
+        current.setPassword(password);
+        return userService.add(current);
+    }
+}
