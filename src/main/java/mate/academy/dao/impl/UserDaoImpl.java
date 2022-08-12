558c42c806cefc378e55db9f3fda6e0b8adc652f
yaroslav <morozov.yrv@gmail.com>
2022-08-13 00:00:09 +0300

    user dao implementation

---
+++ src/main/java/mate/academy/dao/impl/UserDaoImpl.java
@@ -0,0 +1,52 @@
+package mate.academy.dao.impl;
+
+import java.util.Optional;
+import mate.academy.dao.UserDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.model.User;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+public class UserDaoImpl implements UserDao {
+    @Override
+    public User add(User user) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+            return user;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert user" + user, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<User> query = cb.createQuery(User.class);
+            Root<User> userRoot = query.from(User.class);
+            Predicate emailPredicate = cb.equal(userRoot.get("email"), email);
+            query.where(emailPredicate);
+            return Optional.ofNullable(session.createQuery(query).getSingleResult());
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't find user by email: " + email, e);
+        }
+    }
+}
