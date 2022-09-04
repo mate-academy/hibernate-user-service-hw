643efb3218fe38ec7ad84408b61aa4f2d4809ed7
Ilya <zimmboco@gmail.com>
2022-09-04 09:53:46 +0300

    create hash password and salt

--- src/main/java/mate/academy/security/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/security/AuthenticationServiceImpl.java
@@ -1,4 +1,4 @@
-package mate.academy.service.impl;
+package mate.academy.security;
 
 import java.util.Optional;
 import mate.academy.exception.AuthenticationException;
@@ -6,7 +6,6 @@ import mate.academy.exception.RegistrationException;
 import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.User;
-import mate.academy.service.AuthenticationService;
 import mate.academy.service.UserService;
 import mate.academy.util.HashUtil;
 
@@ -17,14 +16,11 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public User login(String email, String password) throws AuthenticationException {
-        Optional<User> userFromDbOptional = userService.findByEmail(email);
-        if (userFromDbOptional.isEmpty()) {
-            throw new AuthenticationException("Can`t authenticate user");
-        }
-        User user = userFromDbOptional.get();
-        String hashPassword = HashUtil.hashPassword(password, user.getSalt());
-        if (user.getPassword().equals(hashPassword)) {
-            return user;
+        Optional<User> userOptional = userService.findByEmail(email);
+        if (userOptional.isPresent() && HashUtil.hashPassword(
+                password, userOptional.get().getSalt()).equals(userOptional.get().getPassword())) {
+
+            return userOptional.get();
         }
         throw new AuthenticationException("Email or password incorrect");
     }
