e043328d2833db8b3ab3bbd3cebb4314eb22a196
Dmytro Lemeshchenko <dimmonishe@gmail.com>
2022-09-05 17:49:48 +0300

    Third commit

--- src/main/java/mate/academy/service/impl/AuthenticationServiseImpl.java
+++ src/main/java/mate/academy/service/impl/AuthenticationServiseImpl.java
@@ -11,7 +11,7 @@ import mate.academy.service.UserService;
 import mate.academy.util.HashUtil;
 
 @Service
-public class AuthenticationServise implements AuthenticationService {
+public class AuthenticationServiseImpl implements AuthenticationService {
     @Inject
     private UserService userService;
 
