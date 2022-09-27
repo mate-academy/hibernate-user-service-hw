e69357c992314d635240a448cae262805ad55e56
Heorhiy Nezbrytskiy <georgiy.nezbritskiy@gmail.com>
2022-09-27 16:36:10 +0300

    add hibernate.cfg.xml. add model User and dao, service layer for model. Add exception, AuthenticationService

---
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,43 @@
+package mate.academy.service.impl;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> byEmail = userService.findByEmail(email);
+        if (byEmail.isPresent() && HashUtil.hashPassword(password, byEmail.get().getSalt())
+                .equals(byEmail.get().getPassword())) {
+            return byEmail.get();
+        }
+        throw new AuthenticationException("Pair 'email - password' is incorect");
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        if (email.isEmpty() || password.isEmpty()) {
+            throw new RegistrationException("Email or password can't be empty");
+        }
+        Optional<User> byEmail = userService.findByEmail(email);
+        if (byEmail.isEmpty()) {
+            User user = new User();
+            user.setEmail(email);
+            user.setPassword(password);
+            userService.add(user);
+            return user;
+        }
+        throw new RegistrationException("User with email: " + email + " exists");
+    }
+}
---
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -0,0 +1,34 @@
+package mate.academy.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class HashUtil {
+    private static final String CRYPTO_ALGORITHM = "SHA-512";
+
+    private HashUtil() {
+    }
+
+    public static String hashPassword(String password, byte[] salt) {
+        StringBuilder hashPassword = new StringBuilder();
+        try {
+            MessageDigest instance = MessageDigest.getInstance(CRYPTO_ALGORITHM);
+            instance.update(salt);
+            byte[] digest = instance.digest(password.getBytes());
+            for (byte b : digest) {
+                hashPassword.append(String.format("%02x", b));
+            }
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Can't create hash using SHA-512 algorithn", e);
+        }
+        return hashPassword.toString();
+    }
+
+    public static byte[] getSalt() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] salt = new byte[16];
+        secureRandom.nextBytes(salt);
+        return salt;
+    }
+}
