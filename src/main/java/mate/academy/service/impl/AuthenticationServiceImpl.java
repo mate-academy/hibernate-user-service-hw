0729b32181fb3994050ae9c2fa7abeaf12026407
GolDenik <denik.golibroda@gmail.com>
2022-10-02 20:50:25 +0300

    did the task and checked common mistakes

---
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,41 @@
+package mate.academy.service.impl;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> optionalUser = userService.findByEmail(email);
+        if (optionalUser.isPresent()
+                && optionalUser.get().getPassword().equals(HashUtil.hashPassword(password,
+                optionalUser.get().getSalt()))) {
+            return optionalUser.get();
+        }
+        throw new AuthenticationException("Wrong credentials, please try again.");
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        Optional<User> optionalUser = userService.findByEmail(email);
+        if (optionalUser.isPresent()) {
+            throw new RegistrationException("This email is unavailable, please try another one.");
+        }
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        userService.add(user);
+        return user;
+    }
+}
