d7a4f5d68841680e6102a1d8b6ad0a1cde5f8ebf
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-10-03 20:18:12 +0200

    implement AuthenticationServiceImpl

---
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,44 @@
+package mate.academy.service.impl;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.model.User;
+import mate.academy.lib.Service;
+import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> userFromDbOptional = userService.findByEmail(email);
+        if (userFromDbOptional.isEmpty()) {
+            throw new AuthenticationException("Can't authenticate user by this data.");
+        }
+        User user = userFromDbOptional.get();
+        String hashPassword = HashUtil.hashPassword(password, user.getSalt());
+        if (user.getPassword().equals(hashPassword)) {
+            return user;
+        }
+        throw new AuthenticationException("Can't authenticate user by this data.");
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        Optional<User> optionalUser = userService.findByEmail(email);
+        if (optionalUser.isPresent()) {
+            throw new RegistrationException("Incorrect data!");
+        }
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        userService.add(user);
+        return user;
+    }
+}
