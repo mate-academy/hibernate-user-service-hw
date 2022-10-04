63afa0ae0a75c32aa5b3ebedd646a93f32a9b836
Ilja Nekrituk <Nekrit99@gmail.com>
2022-10-04 19:24:25 +0300

    Classes added, Service ready

---
+++ src/main/java/mate/academy/model/User.java
@@ -0,0 +1,10 @@
+package mate.academy.model;
+
+import java.math.BigDecimal;
+
+public class User {
+    private Long id;
+    private String username;
+    private String password;
+
+}
---
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -0,0 +1,9 @@
+package mate.academy.util;
+
+public class HashUtil {
+    private static final String CRYPTO_ALGORITHM = "SHA-512";
+
+    private HashUtil() {
+    }
+
+}
