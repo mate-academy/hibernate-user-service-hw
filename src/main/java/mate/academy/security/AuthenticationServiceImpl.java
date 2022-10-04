2f51e0a13c2513e7fed149b4b50038456ee50439
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-10-04 19:37:11 +0300

    AuthenticationService and AuthenticationServiceImpl were added

---
+++ src/main/java/mate/academy/security/AuthenticationServiceImpl.java
@@ -0,0 +1,43 @@
+package mate.academy.security;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> optionalUserFromDb = userService.findByEmail(email);
+        if (optionalUserFromDb.isEmpty()) {
+            throw new AuthenticationException("Can't authenticate user");
+        }
+        User user = optionalUserFromDb.get();
+        String hashedPassword = HashUtil.hashPassword(password, user.getSalt());
+        if (hashedPassword.equals(user.getPassword())) {
+            return user;
+        }
+        throw new AuthenticationException("Can't authenticate user");
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        Optional<User> optionalUserFromDb = userService.findByEmail(email);
+        if (optionalUserFromDb.isEmpty()) {
+            User user = new User();
+            user.setEmail(email);
+            user.setPassword(password);
+            userService.add(user);
+            return user;
+        }
+        throw new RegistrationException("This email has already exist in DB: " + email);
+    }
+}
