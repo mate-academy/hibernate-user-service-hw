b9987888d04412604ee01c23129b583ff1999f29
buuufy <s1mpleworkpost@gmail.com>
2022-10-05 01:09:43 +0300

    add fixes

--- src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -3,6 +3,7 @@ package mate.academy.service.impl;
 import java.util.Optional;
 import javax.naming.AuthenticationException;
 import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.User;
 import mate.academy.service.AuthenticationService;
@@ -11,17 +12,15 @@ import mate.academy.util.HashUtil;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
     private UserService userService;
 
     @Override
     public User login(String email, String password) throws AuthenticationException {
         Optional<User> userFromDbByEmail = userService.findByEmail(email);
-        if (userFromDbByEmail.isEmpty()) {
-            throw new AuthenticationException("Couldn't login");
-        }
         User user = userFromDbByEmail.get();
         String hashedPassword = HashUtil.hashPassword(password, user.getSalt());
-        if (user.getPassword().equals(hashedPassword)) {
+        if (!userFromDbByEmail.isPresent() && user.getPassword().equals(hashedPassword)) {
             return user;
         }
         throw new AuthenticationException("Couldn't login");
@@ -29,6 +28,9 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public User register(String email, String password) throws RegistrationException {
+        if (email.isEmpty() || password.isEmpty()) {
+            throw new RegistrationException("email or password is empty");
+        }
         if (userService.findByEmail(email).isEmpty()) {
             User user = new User(email, password);
             return userService.add(user);
