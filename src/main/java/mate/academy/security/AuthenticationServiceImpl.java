8fec92bf7a71681e07d8927b90ad2b00ffcd2e05
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-10-04 19:52:33 +0300

    method add in UserDaoImpl was changed

--- src/main/java/mate/academy/security/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/security/AuthenticationServiceImpl.java
@@ -17,13 +17,12 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public User login(String email, String password) throws AuthenticationException {
         Optional<User> optionalUserFromDb = userService.findByEmail(email);
-        if (optionalUserFromDb.isEmpty()) {
-            throw new AuthenticationException("Can't authenticate user");
-        }
-        User user = optionalUserFromDb.get();
-        String hashedPassword = HashUtil.hashPassword(password, user.getSalt());
-        if (hashedPassword.equals(user.getPassword())) {
-            return user;
+        if (optionalUserFromDb.isPresent()) {
+            User user = optionalUserFromDb.get();
+            String hashedPassword = HashUtil.hashPassword(password, user.getSalt());
+            if (hashedPassword.equals(user.getPassword())) {
+                return user;
+            }
         }
         throw new AuthenticationException("Can't authenticate user");
     }
