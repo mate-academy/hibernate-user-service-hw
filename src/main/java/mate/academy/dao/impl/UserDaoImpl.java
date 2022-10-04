a5fe5f1e1027126eb3b955e006593066978ab50a
Ilja Nekrituk <Nekrit99@gmail.com>
2022-10-04 22:15:50 +0300

    Changed user

--- src/main/java/mate/academy/dao/impl/UserDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/UserDaoImpl.java
@@ -1,10 +1,15 @@
 package mate.academy.dao.impl;
 
 import mate.academy.dao.UserDao;
+import mate.academy.lib.Dao;
 import mate.academy.model.User;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 import java.util.Optional;
 
+@Dao
 public class UserDaoImpl implements UserDao {
     @Override
     public User add(User user) {
@@ -13,6 +18,11 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public Optional<User> findByEmail(String email) {
-        return Optional.empty();
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("FROM User u WHERE u.email = :email", User.class)
+                    .setParameter("email", email)
+                    .uniqueResultOptional();
+        }
     }
 }
