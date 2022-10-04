f26a7666cf149692021eb17e960034adc0a58e0b
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-10-04 17:43:59 +0300

    UserService and UserServiceImpl were added

--- src/main/java/mate/academy/util/HashUtil.java
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -7,7 +7,7 @@ import java.security.SecureRandom;
 public class HashUtil {
     private static final String CRYPTO_ALGORITHM = "SHA-512";
 
-    public byte[] getSalt() {
+    public static byte[] getSalt() {
         SecureRandom secureRandom = new SecureRandom();
         byte[] salt = new byte[16];
         secureRandom.nextBytes(salt);
