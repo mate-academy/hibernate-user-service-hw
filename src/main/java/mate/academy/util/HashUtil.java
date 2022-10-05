3d92ae778a713f5a838cc13d06dbdb85c96ba117
KosatskiyKirill <kirillkosatskiy@gmail.com>
2022-10-05 16:06:33 +0300

    fix mistakes

--- src/main/java/mate/academy/util/HashUtil.java
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -27,7 +27,7 @@ public class HashUtil {
                 stringBuilder.append(String.format("%02x", b));
             }
         } catch (NoSuchAlgorithmException e) {
-            throw new IllegalStateException("Couldn't create hash using SHA-512 algorithm", e);
+            throw new IllegalStateException("Couldn't create hash using " + CRYPTO_ALGORITHM + " algorithm", e);
         }
         return stringBuilder.toString();
     }
