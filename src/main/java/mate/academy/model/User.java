03ba1b35b8a5425626840ea7531c5bacfa7d545c
fliarkovskyi <fliarkovskyi@gmail.com>
2022-10-05 15:21:06 +0300

    fix style User class

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -2,7 +2,11 @@ package mate.academy.model;
 
 import java.util.Arrays;
 import java.util.Objects;
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class User {
