ebb7ba148b9b8f8eb513fee9f55a3a7926cf3714
Myroslav_Voskoboinyk <maestro.of.pain@gmail.com>
2022-10-05 10:05:24 +0300

    Authentication and Registration were implemented

---
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,43 @@
+package mate.academy.service.impl;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> optionalUser = userService.findByEmail(email);
+        if (optionalUser.isEmpty()
+                || !optionalUser.get().getPassword()
+                .equals(HashUtil.hashPassword(password,
+                        optionalUser.get().getSalt()))) {
+            throw new AuthenticationException("Username or password was incorrect");
+        }
+        return optionalUser.get();
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        if (userService.findByEmail(email).isPresent()) {
+            throw new RegistrationException("User with this email already exists");
+        }
+        if (password.isEmpty()) {
+            throw new RegistrationException("Password must not be empty");
+        }
+        User newUser = new User();
+        newUser.setEmail(email);
+        newUser.setPassword(password);
+        return userService.add(newUser);
+    }
+}
