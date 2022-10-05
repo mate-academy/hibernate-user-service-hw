ee8a75ff52605e9801be70fe6597863f1e9b50fb
EagleAndrew <tortloveduck4@gmail.com>
2022-10-05 14:18:16 +0200

    added @Inject

--- src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -3,6 +3,7 @@ package mate.academy.service.impl;
 import java.util.Optional;
 import mate.academy.exception.AuthenticationException;
 import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.User;
 import mate.academy.service.AuthenticationService;
@@ -11,6 +12,7 @@ import mate.academy.util.HashUtil;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
     private UserService userService;
 
     @Override
