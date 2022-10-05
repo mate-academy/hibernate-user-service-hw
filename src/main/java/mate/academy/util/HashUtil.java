9e68579471ee56dc6b391a3cab8db6f2a9925adb
KosatskiyKirill <kirillkosatskiy@gmail.com>
2022-10-05 16:08:49 +0300

    mvn clean package done

--- src/main/java/mate/academy/util/HashUtil.java
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -27,7 +27,8 @@ public class HashUtil {
                 stringBuilder.append(String.format("%02x", b));
             }
         } catch (NoSuchAlgorithmException e) {
-            throw new IllegalStateException("Couldn't create hash using " + CRYPTO_ALGORITHM + " algorithm", e);
+            throw new IllegalStateException("Couldn't create hash using "
+                    + CRYPTO_ALGORITHM + " algorithm", e);
         }
         return stringBuilder.toString();
     }
