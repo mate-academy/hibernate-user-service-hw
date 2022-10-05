e59eb27801b8520df3a3530698cf7eea6d927325
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-10-05 14:22:20 +0300

    catch exception

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -8,12 +8,18 @@ import mate.academy.security.AuthenticationService;
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.academy");
 
-    public static void main(String[] args) throws RegistrationException, AuthenticationException {
+    public static void main(String[] args) {
         final AuthenticationService authenticationService =
                 (AuthenticationService) injector.getInstance(AuthenticationService.class);
         String email = "bob@asd.com";
         String password = "b1234567";
-        authenticationService.register(email, password);
-        System.out.println(authenticationService.login(email, password));
+        try {
+            authenticationService.register(email, password);
+            System.out.println(authenticationService.login(email, password));
+        } catch (RegistrationException e) {
+            throw new RuntimeException("Can't register user with email: " + email, e);
+        } catch (AuthenticationException e) {
+            throw new RuntimeException("Can't login user with email: " + email, e);
+        }
     }
 }
