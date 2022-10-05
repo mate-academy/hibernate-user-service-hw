9f5305c0d9de6142ad642f2f4c45446017778446
IraHurko <iragurko19@gmail.com>
2022-10-05 15:32:42 +0300

    Fix checkstyle

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -1,7 +1,5 @@
 package mate.academy.model;
 
-import java.util.Arrays;
-import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
