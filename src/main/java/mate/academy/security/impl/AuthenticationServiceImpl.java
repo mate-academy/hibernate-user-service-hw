c0f66e026f9c045590e1b656fbe5d0748d0a52bc
Maksym Korkosh <makskorkosh@gmail.com>
2022-10-05 08:26:53 +0300

    moved authentication service to security package

--- src/main/java/mate/academy/security/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/security/impl/AuthenticationServiceImpl.java
@@ -1,4 +1,4 @@
-package mate.academy.service.impl;
+package mate.academy.security.impl;
 
 import java.util.Optional;
 import mate.academy.exception.AuthenticationException;
@@ -6,7 +6,7 @@ import mate.academy.exception.RegistrationException;
 import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.User;
-import mate.academy.service.AuthenticationService;
+import mate.academy.security.AuthenticationService;
 import mate.academy.service.UserService;
 import mate.academy.util.HashUtil;
 
