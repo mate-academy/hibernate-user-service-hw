9966cc4b876283d5ef747d7d5bc1e30ad113a2fa
Ihor Dmytrush <ihor.dm.a@gmail.com>
2022-11-02 17:41:43 +0200

    1st try

---
+++ src/main/java/mate/academy/security/AuthenticationServiceImpl.java
@@ -0,0 +1,39 @@
+package mate.academy.security;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> user = userService.findByEmail(email);
+        if (!user.isEmpty()
+                && user.get().getPassword()
+                .equals(HashUtil.hashPassword(password, user.get().getSalt()))) {
+            return user.get();
+        }
+        throw new AuthenticationException("Authentication failed!");
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        if ((email == null)
+                || (password == null)
+                || (email != null && email.isEmpty())
+                || (password != null && password.isEmpty())
+                || !userService.findByEmail(email).isEmpty()) {
+            throw new RegistrationException("Can't register new user");
+        }
+        return userService.add(new User(email, password));
+    }
+}
