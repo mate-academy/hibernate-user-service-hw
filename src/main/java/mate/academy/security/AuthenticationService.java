c5252ea3b842edf04f7dc3ad245ffa0786609db3
Yura Hryniv <superyura444@gmail.com>
2022-11-25 09:49:38 +0200

    UserService is implemented

---
+++ src/main/java/mate/academy/security/AuthenticationService.java
@@ -0,0 +1,10 @@
+package mate.academy.security;
+
+import mate.academy.exception.AuthenticationException;
+import mate.academy.model.User;
+
+public interface AuthenticationService {
+    User register(String login, String password);
+
+    User login(String login, String password) throws AuthenticationException;
+}
---
+++ src/main/java/mate/academy/security/AuthenticationServiceImpl.java
@@ -0,0 +1,37 @@
+package mate.academy.security;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User register(String login, String password) {
+        User user = new User();
+        user.setPassword(password);
+        user.setLogin(login);
+        return userService.add(user);
+    }
+
+    @Override
+    public User login(String login, String password) throws AuthenticationException {
+        Optional<User> userFromDbOptional = userService.findByEmail(login);
+        if (userFromDbOptional.isEmpty()) {
+            throw new AuthenticationException("Can't authenticate user");
+        }
+        User user = userFromDbOptional.get();
+        String hashedPassword = HashUtil.hashPassword(password, user.getSalt());
+        if (user.getPassword().equals(hashedPassword)) {
+            return user;
+        }
+        throw new AuthenticationException("Can't authenticate user");
+    }
+}
