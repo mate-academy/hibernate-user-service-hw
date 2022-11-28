3f5c4cc8410872f08c467c8c29c92fff9920eaeb
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-11-28 12:06:43 +0200

    Remove redundant try catch blocks

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -7,24 +7,16 @@ import mate.academy.model.User;
 import mate.academy.service.AuthenticationService;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws AuthenticationException, RegistrationException {
         final Injector injector
                 = Injector.getInstance("mate.academy");
         AuthenticationService authenticationService
                 = (AuthenticationService) injector.getInstance(AuthenticationService.class);
         User loginUser = null;
-        try {
-            loginUser = authenticationService.login("test@test.com", "TESTtest!2");
-        } catch (AuthenticationException e) {
-            throw new RuntimeException(e);
-        }
+        loginUser = authenticationService.login("test@test.com", "TESTtest!2");
         System.out.println(loginUser);
         User registerUser = null;
-        try {
-            registerUser = authenticationService.register("new@new.com", "PaSSWord!231!");
-        } catch (RegistrationException e) {
-            throw new RuntimeException(e);
-        }
+        registerUser = authenticationService.register("new@new.com", "PaSSWord!231!");
         System.out.println(registerUser);
     }
 }
