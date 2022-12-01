9cfa504f513f8d69ab442c7791d2c0e588f3b866
Anastasia Vasylyk <anastasiavasylykv@gmail.com>
2022-12-01 14:23:27 +0200

    Added authentication service and user service

---
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,42 @@
+package mate.academy.service.impl;
+
+import java.util.Objects;
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> userFromDB = userService.findByEmail(email);
+        User user = userFromDB.get();
+        String hashedPassword = HashUtil.hashPassword(password, user.getSalt());
+        if (userFromDB.isEmpty()
+                || !Objects.equals(hashedPassword, user.getPassword())) {
+            throw new AuthenticationException("Invalid login or password!");
+        }
+        return user;
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        Optional<User> userFromDB = userService.findByEmail(email);
+        if (userFromDB.isPresent()) {
+            throw new RegistrationException("User with this login already exists!");
+        }
+        if (password.length() < 6) {
+            throw new RegistrationException("Password length cannot be less than 6 symbols!");
+        }
+        return userService.add(new User(email, password));
+    }
+}
