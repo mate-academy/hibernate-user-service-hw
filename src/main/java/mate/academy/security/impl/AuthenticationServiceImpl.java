50f3e5fa6ad49f5eea22c1a12799fb71eba4b5a6
Volodymyr Oliinyk <volodymyr.oliinyk89@gmail.com>
2022-12-02 15:54:03 +0200

    added fixes after review

--- src/main/java/mate/academy/security/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/security/impl/AuthenticationServiceImpl.java
@@ -12,28 +12,50 @@ import mate.academy.util.HashUtil;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final int PASSWORD_MIN_LENGTH = 8;
+    private static final String REGEX_PATTERN = "^(.+)@(\\w+)$";
     @Inject
     private UserService userService;
 
     @Override
     public User login(String email, String password) throws AuthenticationException {
-        Optional<User> userOptional = userService.findByEmail(email);
-        if (userOptional.isPresent()) {
-            User user = userOptional.get();
-            String hashedPassword = HashUtil.hashPassword(password, user.getSalt());
-            if (user.getPassword().equals(hashedPassword)) {
-                return user;
-            }
+        Optional<User> userFromDB = userService.findByEmail(email);
+        if (userFromDB.isPresent() && matchPasswords(email, userFromDB.get())) {
+            return userFromDB.get();
         }
         throw new AuthenticationException("Can't authenticate user by email " + email);
     }
 
     @Override
     public User register(String email, String password) throws RegistrationException {
+        validateUser(email, password);
+        return userService.add(new User(email, password));
+    }
+
+    private void validateUser(String email, String password) throws RegistrationException {
         Optional<User> userOptional = userService.findByEmail(email);
         if (userOptional.isPresent()) {
             throw new RegistrationException("Can't register user with email " + email);
         }
-        return userService.add(new User(email, password));
+        if (!isValidEmail(email)) {
+            throw new RegistrationException("Not valid email format " + email);
+        }
+        if (!isValidPassword(password)) {
+            throw new RegistrationException("Password length must be min "
+                    + PASSWORD_MIN_LENGTH + " symbols");
+        }
+    }
+
+    private boolean matchPasswords(String password, User userFromDb) {
+        String hashedPassword = HashUtil.hashPassword(password, userFromDb.getSalt());
+        return hashedPassword.equals(userFromDb.getPassword());
+    }
+
+    private boolean isValidEmail(String email) {
+        return email.matches(REGEX_PATTERN);
+    }
+
+    private boolean isValidPassword(String password) {
+        return password.length() >= PASSWORD_MIN_LENGTH;
     }
 }
