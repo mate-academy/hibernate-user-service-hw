8d55a5ffc7e2f9b962962a920c03a7c2c823e785
Maksym Shveda <mshveda28@gmail.com>
2022-12-05 18:29:10 +0200

    Add Table annotations to model corresponding to existent db

--- src/main/java/mate/academy/model/MovieSession.java
+++ src/main/java/mate/academy/model/MovieSession.java
@@ -7,8 +7,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "MovieSession")
 public class MovieSession {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
