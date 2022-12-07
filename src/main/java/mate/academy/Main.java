346e768e699b9d997bd1d9e3b6ff2331ec836f5c
Bohdan Senyk <senyk2001@gmail.com>
2022-12-07 12:41:00 +0200

    added empty line

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -16,6 +16,7 @@ import mate.academy.service.MovieSessionService;
 
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) throws RegistrationException, AuthenticationException {
         MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
 
