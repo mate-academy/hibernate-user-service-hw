c5e05685f2c8f6291e9a7567e829122cbd7112c7
Fokin <Fokin1@ukr.net>
2022-12-08 05:56:37 +0100

    create class User, UserDaoImpl and interface UserDao, implements mathod add, get, getAll for user

---
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -0,0 +1,25 @@
+package mate.academy.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class HashUtil {
+    private static final String CRYPTO_ALGORITHM = "SHA-512";
+
+    public HashUtil() {
+    }
+
+    public static String hashPassword(String password) {
+        StringBuilder hashedPassword = new StringBuilder();
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance(CRYPTO_ALGORITHM);
+            byte[] digest = messageDigest.digest(password.getBytes());
+            for (byte b : digest) {
+                hashedPassword.append(String.format("%02x", b));
+            }
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException("Could not create hash using SHA-512 algorithm", e);
+        }
+        return hashedPassword.toString();
+    }
+}
