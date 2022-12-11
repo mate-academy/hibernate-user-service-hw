58f2b4915b673562ad2e4cacef2cb209a6ad4709
Andrii Matveev <matveev.tesla@gmail.com>
2022-12-11 16:32:14 +0200

    authentication_service check was added

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -2,10 +2,14 @@ package mate.academy;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
 import mate.academy.lib.Injector;
 import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
 import mate.academy.model.MovieSession;
+import mate.academy.model.User;
+import mate.academy.security.AuthenticationService;
 import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
@@ -13,7 +17,7 @@ import mate.academy.service.MovieSessionService;
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.academy");
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws RegistrationException, AuthenticationException {
         MovieService movieService = (MovieService)
                 injector.getInstance(MovieService.class);
 
@@ -57,5 +61,14 @@ public class Main {
         System.out.println(movieSessionService.get(yesterdayMovieSession.getId()));
         System.out.println(movieSessionService.findAvailableSessions(
                         fastAndFurious.getId(), LocalDate.now()));
+
+        User user = new User();
+        user.setEmail("1234@mail.com");
+        user.setPassword("qwerty");
+
+        AuthenticationService authenticationService =
+                (AuthenticationService) injector.getInstance(AuthenticationService.class);
+        authenticationService.register(user.getEmail(), user.getPassword());
+        System.out.println(authenticationService.login(user.getEmail(), user.getPassword()));
     }
 }
