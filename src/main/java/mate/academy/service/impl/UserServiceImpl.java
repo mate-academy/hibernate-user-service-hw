523546825e8a1030b62f7059ad6243a5a6d54030
AndriySichniy <sichniy.andriy@gmail.com>
2023-01-11 14:06:19 +0200

    add annotations

--- src/main/java/mate/academy/service/impl/UserServiceImpl.java
+++ src/main/java/mate/academy/service/impl/UserServiceImpl.java
@@ -2,10 +2,14 @@ package mate.academy.service.impl;
 
 import java.util.Optional;
 import mate.academy.dao.UserDao;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
 import mate.academy.model.User;
 import mate.academy.service.UserService;
 
+@Service
 public class UserServiceImpl implements UserService {
+    @Inject
     private UserDao userDao;
     @Override
     public User add(User user) {
