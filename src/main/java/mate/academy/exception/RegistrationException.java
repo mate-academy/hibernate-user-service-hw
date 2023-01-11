a9462fa087ed86d2a5e6ace07252c9439a8c4503
AndriySichniy <89684976+SichniyAndriy@users.noreply.github.com>
2023-01-11 18:06:54 +0200

    Update src/main/java/mate/academy/exception/RegistrationException.java
    Co-authored-by: Anastasia Len <81781479+aaananas@users.noreply.github.com>

--- src/main/java/mate/academy/exception/RegistrationException.java
+++ src/main/java/mate/academy/exception/RegistrationException.java
@@ -1,7 +1,7 @@
 package mate.academy.exception;
 
 public class RegistrationException extends Exception {
-    public RegistrationException(String message, Throwable cause) {
+    public RegistrationException(String message) {
         super(message, cause);
     }
 }
