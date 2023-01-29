2bc16474f16f6b62f3cb933c16eedfe98c0bc083
Anton Kalenskyi <anton_kalenskiy@ukr.net>
2023-01-29 20:41:19 +0200

    Created model User, added UserDao interface with some methods and implementation. Also added UserService, AuthenticationService and implementation to them.Besides this, created checked AuthenticationException and RegistrationException.

---
+++ src/main/java/mate/academy/security/AuthenticationServiceImpl.java
@@ -0,0 +1,43 @@
+package mate.academy.security;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        if (checkUserEmail(email).isPresent()) {
+            User user = checkUserEmail(email).get();
+            String hashedPassword = HashUtil.hashPassword(password, user.getSalt());
+            if (user.getPassword().equals(hashedPassword)) {
+                return user;
+            }
+        }
+        throw new AuthenticationException("Can not authenticate user with email: " + email);
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        if (checkUserEmail(email).isPresent()) {
+            throw new RegistrationException("Can not register user with this email: " + email);
+        }
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        return userService.add(user);
+    }
+
+    private Optional<User> checkUserEmail(String email) {
+        return userService.findByEmail(email);
+    }
+}
