f8694e0875128b004b83b65c92657f39a865c5e7
Maksim Surzhik <kabaneri15@gmail.com>
2023-02-07 14:21:21 +0200

    done

---
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,35 @@
+package mate.academy.service.impl;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> userByEmail = userService.findByEmail(email);
+        if (userByEmail.isEmpty() || !HashUtil.getHash(password, userByEmail.get().getSalt())
+                .equals(userByEmail.get().getPassword())) {
+            throw new AuthenticationException("Can't authenticate User");
+        }
+        return userByEmail.get();
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        if (userService.findByEmail(email).isPresent()) {
+            throw new RegistrationException("Can't create existed User");
+        }
+        return userService.add(new User(email, password));
+    }
+}
---
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -0,0 +1,34 @@
+package mate.academy.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class HashUtil {
+    private static final String CRYPTO_ALGORITHM = "SHA-256";
+
+    public HashUtil() {
+    }
+
+    public static String getHash(String pas, byte[] salt) {
+        StringBuilder builder = new StringBuilder();
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance(CRYPTO_ALGORITHM);
+            messageDigest.update(salt);
+            byte[] digest = messageDigest.digest(pas.getBytes());
+            for (byte b: digest) {
+                builder.append(String.format("%x", b));
+            }
+            return builder.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Can't get a hash value", e);
+        }
+    }
+
+    public static byte[] getSalt() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] salt = new byte[8];
+        secureRandom.nextBytes(salt);
+        return salt;
+    }
+}
