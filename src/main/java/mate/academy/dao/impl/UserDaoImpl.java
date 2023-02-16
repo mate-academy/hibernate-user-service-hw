684029d9208bf9528d19f6eff9d9f8e8aa223843
Ihor Vynogradskyi <avassorv@gmail.com>
2023-02-16 11:15:07 +0200

    hiber user 4

--- src/main/java/mate/academy/dao/impl/UserDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/UserDaoImpl.java
@@ -7,19 +7,16 @@ import mate.academy.lib.Dao;
 import mate.academy.model.User;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 @Dao
 public class UserDaoImpl implements UserDao {
-    private SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-
     @Override
     public User add(User user) {
         Session session = null;
         Transaction transaction = null;
         try {
-            session = sessionFactory.openSession();
+            session = HibernateUtil.getSessionFactory().openSession();
             transaction = session.beginTransaction();
             session.save(user);
             transaction.commit();
@@ -38,7 +35,7 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public Optional<User> findByEmail(String email) {
-        try (Session session = sessionFactory.openSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             return session.createQuery("from User u where u.email = : email", User.class)
                     .setParameter("email", email)
                     .uniqueResultOptional();
