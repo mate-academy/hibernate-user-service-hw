9cb73775c814f7a4f54973650f6ffaf2ea0c6407
Your NameMykola <mykola.osolinskyi@gmail.com>
2023-02-17 14:39:52 +0100

    fixed comments after student review

--- src/main/java/mate/academy/security/AuthenticationService.java
+++ src/main/java/mate/academy/security/AuthenticationService.java
@@ -5,7 +5,6 @@ import mate.academy.exception.RegistrationException;
 import mate.academy.model.User;
 
 public interface AuthenticationService {
-
     User login(String email, String password) throws AuthenticationException;
 
     User register(String email, String password) throws RegistrationException;
