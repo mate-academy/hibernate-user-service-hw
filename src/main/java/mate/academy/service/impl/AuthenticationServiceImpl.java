ee66fea64c892f039fe5dc6be4d3d1f740d48090
Dastya <daniillipenec@gmail.com>
2023-02-17 15:26:26 +0200

    Implemented and tested in main

---
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,36 @@
+package mate.academy.service.impl;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> userOptional = userService.findByEmail(email);
+        if (userOptional.isEmpty()
+                || !HashUtil.getHashedPassword(password, userOptional.get().getSalt())
+                .equals(userOptional.get().getPassword())) {
+            throw new AuthenticationException("Your Email or password are invalid");
+        }
+        return userOptional.get();
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        if (userService.findByEmail(email).isPresent()) {
+            throw new RegistrationException("User already exists with email " + email);
+        }
+        return userService.add(new User(email, password));
+    }
+}
