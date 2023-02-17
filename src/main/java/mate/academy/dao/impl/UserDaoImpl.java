f87af97ca10288e9935b6d6a5a669f2e3a0b2d76
ydikalo <dikalotel@gmail.com>
2023-02-17 12:08:17 +0200

    implemented task: main is working; mvn clean package - build success.

---
+++ src/main/java/mate/academy/dao/impl/UserDaoImpl.java
@@ -0,0 +1,46 @@
+package mate.academy.dao.impl;
+
+import java.util.Optional;
+import mate.academy.dao.UserDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.User;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+@Dao
+public class UserDaoImpl implements UserDao {
+    @Override
+    public User add(User user) {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+            return user;
+        } catch (HibernateException e) {
+            if (session != null) {
+                session.getTransaction().rollback();
+            }
+            throw new DataProcessingException("Can't insert user " + user, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<User> query = session.createQuery("from User where email = :email", User.class);
+            query.setParameter("email", email);
+            return query.uniqueResultOptional();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't find user by email: " + email, e);
+        }
+    }
+}
---
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,63 @@
+package mate.academy.service.impl;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final String EMAIL_REGEX = ".+@\\w+\\.\\w+";
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> userFromDbOptional = userService.findByEmail(email);
+        if (userFromDbOptional.isPresent() && checkPassword(userFromDbOptional.get(), password)) {
+            return userFromDbOptional.get();
+        }
+        throw new AuthenticationException("Email or password is invalid");
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        validateData(email, password);
+        return userService.add(new User(email, password));
+    }
+
+    private void validateData(String email, String password) throws RegistrationException {
+        if (!isPasswordValid(password)) {
+            throw new RegistrationException("Invalid password.");
+        }
+        if (!isEmailValid(email)) {
+            throw new RegistrationException("Invalid email format");
+        }
+        if (!isEmailUnique(email)) {
+            throw new RegistrationException("User is already registered with email " + email);
+        }
+    }
+
+    private boolean checkPassword(User user, String password) {
+        String hashedPassword = HashUtil.hashPassword(password, user.getSalt());
+        return user.getPassword().equals(hashedPassword);
+    }
+
+    private boolean isPasswordValid(String password) {
+        return !(password == null || password.isEmpty());
+    }
+
+    private boolean isEmailValid(String email) {
+        return email != null && email.matches(EMAIL_REGEX);
+    }
+
+    private boolean isEmailUnique(String email) {
+        Optional<User> optionalUser = userService.findByEmail(email);
+        return optionalUser.isEmpty();
+    }
+}
