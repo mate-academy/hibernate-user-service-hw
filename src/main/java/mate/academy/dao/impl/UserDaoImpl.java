8a328df1a2e7dc0c90a1d1e7164618e5fbc6c6e3
Roman Yuskevych <yuskevych@gmail.com>
2023-02-23 23:02:43 +0200

    added fixes after review

--- src/main/java/mate/academy/dao/impl/UserDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/UserDaoImpl.java
@@ -8,7 +8,6 @@ import mate.academy.model.User;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-import org.hibernate.query.Query;
 
 @Dao
 public class UserDaoImpl implements UserDao {
@@ -37,10 +36,11 @@ public class UserDaoImpl implements UserDao {
     @Override
     public Optional<User> findByEmail(String email) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            Query<User> query = session.createQuery("from User u where u.email = :email",
-                    User.class);
-            query.setParameter("email", email);
-            return query.uniqueResultOptional();
+            return session.createQuery("from User u where u.email = :email", User.class)
+                    .setParameter("email", email)
+                    .uniqueResultOptional();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't find user by email=" + email, e);
         }
     }
 }
