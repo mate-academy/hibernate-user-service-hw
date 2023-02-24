30472e5c32b82b39cfe1cc2b303b7a31d0481ec8
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2023-02-24 09:02:21 +0200

    Implemented UserDaoImpl

--- src/main/java/mate/academy/dao/impl/UserDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/UserDaoImpl.java
@@ -2,29 +2,67 @@ package mate.academy.dao.impl;
 
 import java.util.List;
 import java.util.Optional;
+import javax.persistence.criteria.CriteriaQuery;
 import mate.academy.dao.UserDao;
+import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Dao;
 import mate.academy.model.User;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 @Dao
 public class UserDaoImpl implements UserDao {
     @Override
     public User add(User user) {
-        return null;
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't save user: " + user, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return user;
     }
 
     @Override
     public Optional<User> get(Long id) {
-        return Optional.empty();
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return Optional.ofNullable(session.get(User.class, id));
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get user by id: " + id, e);
+        }
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            CriteriaQuery<User> query = session.getCriteriaBuilder()
+                    .createQuery(User.class);
+            query.from(User.class);
+            return session.createQuery(query).getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get all users from Db", e);
+        }
     }
 
     @Override
     public Optional<User> findByLogin(String login) {
-        return Optional.empty();
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("from User u where u.login = :login", User.class)
+                    .setParameter("login", login).uniqueResultOptional();
+        }
     }
 }
