bfb39b9892847fb3111713c3b33aed85fe3861ec
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2023-02-24 17:04:05 +0200

    Implemented methods `login` and `register` in `AuthenticationServiceImpl`, created constructor in User.class

--- src/main/java/mate/academy/security/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/security/impl/AuthenticationServiceImpl.java
@@ -1,18 +1,40 @@
 package mate.academy.security.impl;
 
+import java.util.Optional;
 import mate.academy.exception.AuthenticationException;
 import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
 import mate.academy.model.User;
 import mate.academy.security.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
 
+@Service
 public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
     @Override
     public User login(String email, String password) throws AuthenticationException {
-        return null;
+        Optional<User> userOptionalFromDb = userService.findByEmail(email);
+        if (userOptionalFromDb.isEmpty()) {
+            throw new AuthenticationException("Email or password was incorrect");
+        }
+        User user = userOptionalFromDb.get();
+        String hashPassword = HashUtil.hashedPassword(password, user.getSalt());
+        if (user.getPassword().equals(hashPassword)) {
+            return user;
+        }
+        throw new AuthenticationException("Email or password was incorrect");
     }
 
     @Override
     public User register(String email, String password) throws RegistrationException {
-        return null;
+        if (userService.findByEmail(email).isPresent()) {
+            throw new RegistrationException("Email already exist: " + email);
+        }
+        User user = new User(email, password);
+        return userService.add(user);
     }
 }
