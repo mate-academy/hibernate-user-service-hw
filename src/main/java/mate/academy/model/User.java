d9e791e78c409e4da3671f85536791b890fd280e
ivan-vovnyanko <moooonkit@gmail.com>
2023-03-08 22:14:54 +0200

    deleted unused imports

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -1,12 +1,10 @@
 package mate.academy.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 
 @Entity
 @Table(name = "users")
