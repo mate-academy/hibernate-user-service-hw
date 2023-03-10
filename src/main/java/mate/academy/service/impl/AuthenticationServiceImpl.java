aaf218a1afcc338abf2ce4145c3667476064ca81
Denis Pugovkin <den.pugovkin@gmail.com>
2023-03-10 14:33:03 +0200

    First commit. 1. Model User was created. 2. DAO interface and methods were created. 3. AuthenticationService was created with register and login methods. 4. Own exception were created for authentication. 5. Hashing was used for saving password. 6. All method were tested in Main.

---
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,52 @@
+package mate.academy.service.impl;
+
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final int MIN_PASSWORD_LENGTH = 8;
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        User user = userService.findByEmail(email)
+                .orElseThrow(() -> new AuthenticationException("Please "
+                        + "enter correct email or password"));
+        if (user.getPassword().equals(HashUtil.hashPassword(password, user.getSalt()))) {
+            return user;
+        }
+        throw new AuthenticationException("Please enter correct email or password");
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        if (password.length() < MIN_PASSWORD_LENGTH) {
+            throw new RegistrationException("Password less than 8 symbols");
+        }
+        if (password.contains("[/^[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]*$/]")) {
+            throw new RegistrationException("Password must contain only letters or figures,"
+                    + "any special symbols, please");
+        }
+        if (email == null) {
+            throw new RegistrationException("You need to add correct email");
+        }
+        if (userService.findByEmail(email).isPresent()) {
+            throw new RegistrationException("This login was already registered, "
+                    + "please, choose another one");
+        }
+        User user = new User();
+        user.setEmail(email);
+        user.setSalt(HashUtil.getSalt());
+        user.setPassword(HashUtil.hashPassword(password, user.getSalt()));
+        userService.add(user);
+        return user;
+    }
+}
