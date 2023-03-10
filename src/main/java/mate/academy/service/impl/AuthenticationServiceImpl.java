fcbc03cc221b6670e971d9186d9aafe79d955945
Dmytro Busyhin <zza4etik@gmail.com>
2023-03-10 22:48:02 +0200

    Adds User and corresponding Dao and Service, also implemented Authentication and Registration of the user.

---
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,41 @@
+package mate.academy.service.impl;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> userOptional = userService.findByEmail(email);
+        if (userOptional.isEmpty() || !HashUtil.hashPassword(password, userOptional.get().getSalt())
+                .equals(userOptional.get().getPassword())) {
+            throw new AuthenticationException("Can't found a user with this email and password");
+        }
+        return userOptional.get();
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        if (userService.findByEmail(email).isPresent()) {
+            throw new RegistrationException("A user with this email address already exists");
+        } else if (password.isEmpty()) {
+            throw new RegistrationException("Incorrect password format");
+        }
+        User user = new User();
+        user.setEmail(email);
+        user.setSalt(HashUtil.getSalt());
+        user.setPassword(HashUtil.hashPassword(password, user.getSalt()));
+        return userService.add(user);
+    }
+}
