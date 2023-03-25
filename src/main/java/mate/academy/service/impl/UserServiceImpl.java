716f16853252e0ed7b5b202f39b8a8cdf6f0426d
“Ymishok321 <kkoiku3@gmail.com>
2023-03-25 23:09:23 +0200

    Removed unused import

--- src/main/java/mate/academy/service/impl/UserServiceImpl.java
+++ src/main/java/mate/academy/service/impl/UserServiceImpl.java
@@ -6,7 +6,6 @@ import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.User;
 import mate.academy.service.UserService;
-import mate.academy.util.HashUtil;
 
 @Service
 public class UserServiceImpl implements UserService {
