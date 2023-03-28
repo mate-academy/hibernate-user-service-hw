09530f71f5628f5ee2f7870207a06f2a5b648e83
Andrii Pylypenko <pilipenko1975@gmail.com>
2023-03-28 19:21:46 +0300

    Hibernate_User_Service v.1.0.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -14,7 +14,7 @@ import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
 
 public class Main {
-    private static Injector injector = Injector.getInstance("mate");
+    private static final Injector injector = Injector.getInstance("mate");
 
     public static void main(String[] args) {
         AuthenticationService authenticationService = (AuthenticationService)
