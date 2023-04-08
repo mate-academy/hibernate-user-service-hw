b3a451d7852fa958d9e5c6872b6ddac0bf948096
Vadym Khodorovskyi <vadimhodor@gmail.com>
2023-04-08 19:17:39 +0300

    create User

--- src/main/java/mate/academy/util/HashUtil.java
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -2,11 +2,19 @@ package mate.academy.util;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
 public class HashUtil {
     private static final String CRYPTO_ALGORITHM = "SHA-512";
 
     private HashUtil() {
+    }
+
+    public static byte[] getSalt() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] salt = new byte[16];
+        secureRandom.nextBytes(salt);
+        return salt;
     }
 
     public static String hashPassword(String password, byte [] salt) {
