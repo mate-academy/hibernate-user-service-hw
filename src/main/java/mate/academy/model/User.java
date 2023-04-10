befcf9162bc3489efc391c29bbdf3a63408b24a8
Sofiia <sofiakazzz@gmail.com>
2023-04-10 23:06:28 +0300

    Was implemented userDao, userService and AuthenticationService

---
+++ src/main/java/mate/academy/model/User.java
@@ -0,0 +1,75 @@
+package mate.academy.model;
+
+import java.util.Arrays;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String password;
+    private String login;
+    private byte[] salt;
+
+    public User() {
+    }
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public byte[] getSalt() {
+        return salt;
+    }
+
+    public void setSalt(byte[] salt) {
+        this.salt = salt;
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "id="
+                + id
+                + ", password='"
+                + password
+                + '\''
+                + ", login='"
+                + login
+                + '\''
+                + ", salt="
+                + Arrays.toString(salt)
+                + '}';
+    }
+}
---
+++ src/main/java/mate/academy/security/AuthenticationServiceImpl.java
@@ -0,0 +1,41 @@
+package mate.academy.security;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+
+    public User register(String login, String password)
+            throws RegistrationException {
+        if (userService.findByEmail(login).isEmpty() && !password.isEmpty()) {
+            User user = new User(login, password);
+            return userService.add(user);
+        }
+        throw new RegistrationException("Can not register user cause"
+                + " password or login is incorrect");
+    }
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> userFromDbOptional = userService.findByEmail(email);
+        String passwordFromUser = HashUtil.hashPassword(password,
+                userFromDbOptional.get().getSalt());
+        String hashedPassword = userFromDbOptional.get().getPassword();
+        if (userFromDbOptional.isEmpty() || !passwordFromUser.equals(hashedPassword)) {
+            throw new AuthenticationException("Login data is not valid");
+        }
+
+        return userFromDbOptional.get();
+    }
+}
