d8125a143bf6d351f3a54302d4973fbde46aec77
Sofiia <sofiakazzz@gmail.com>
2023-04-11 20:25:04 +0300

    Was added  @Column(unique = true)

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -1,5 +1,6 @@
 package mate.academy.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,6 +14,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String password;
+    @Column(unique = true)
     private String email;
     private byte[] salt;
 
