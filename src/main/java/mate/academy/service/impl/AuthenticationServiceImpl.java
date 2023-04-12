df7594a3c126f22e6f74e882f860dee657746a48
Sergii Uglovyi <s.uglovyi@gmail.com>
2023-04-12 14:29:50 +0300

    add solution

---
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,44 @@
+package mate.academy.service.impl;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> userFromDbOptional = userService.findByEmail(email);
+        if (userFromDbOptional.isPresent()) {
+            User userFromDB = userFromDbOptional.get();
+            String passwordFromDb = userFromDB.getPassword();
+            String hashPassword = HashUtil.hashPassword(password, userFromDB.getSalt());
+            boolean isValidPassword = !password.isEmpty() && hashPassword.equals(passwordFromDb);
+            if (isValidPassword) {
+                return userFromDB;
+            }
+        }
+        throw new AuthenticationException("Cant authenticate user " + email);
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        Optional<User> userFromDbOptional = userService.findByEmail(email);
+        if (userFromDbOptional.isPresent()) {
+            throw new RegistrationException("User " + email + " is present in database");
+        }
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        return userService.add(user);
+    }
+}
