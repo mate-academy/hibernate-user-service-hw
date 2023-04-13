0eb9d5760d75428b2b281530c687b91c5cf2327b
Yehor-Cherkashyn <gooooodvin1995@gmail.com>
2023-04-13 13:47:53 +0300

    Fixed some mistakes

--- src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -1,4 +1,4 @@
-package mate.academy.security;
+package mate.academy.service.impl;
 
 import java.util.Optional;
 import java.util.regex.Matcher;
@@ -8,6 +8,7 @@ import mate.academy.exception.RegistrationException;
 import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
 import mate.academy.service.UserService;
 import mate.academy.util.HashUtil;
 
@@ -16,27 +17,23 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private static final String EMAIL_PATTERN =
             "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
                     + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-    private static final Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+    private static final String PASSWORD_PATTERN =
+            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,}$";
+    private final Pattern pattern = Pattern.compile(EMAIL_PATTERN);
     @Inject
     private UserService userService;
 
     @Override
     public User login(String email, String password) throws AuthenticationException {
         final Optional<User> userFromDb = userService.findByEmail(email);
-        if (userFromDb.isEmpty()
-                || !userFromDb.get().getPassword()
-                .equals(HashUtil.hashPassword(password, userFromDb.get().getSalt()))) {
-            throw new AuthenticationException("Can't authenticate user by user email: "
-                    + email + " or password");
-        }
+        isEmptyEmailOrInvalidPassword(userFromDb, email, password);
         return userFromDb.get();
     }
 
     @Override
     public User register(String email, String password) throws RegistrationException {
         User user = new User();
-        if (userService.findByEmail(email).isEmpty()
-                && isValidEmail(email)
+        if (isValidEmail(email)
                 && isValidPassword(password)) {
             user.setEmail(email);
             user.setPassword(password);
@@ -46,14 +43,25 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                 + email + " or password");
     }
 
+    private boolean isEmptyEmailOrInvalidPassword(Optional<User> user,
+                                                  String email, String password)
+            throws AuthenticationException {
+        if (user.isEmpty()
+                || !user.get().getPassword()
+                .equals(HashUtil.hashPassword(password, user.get().getSalt()))) {
+            throw new AuthenticationException("Can't authenticate user by user email: "
+                    + email + " or password");
+        }
+        return false;
+    }
+
     private boolean isValidEmail(String email) {
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
     }
 
     public static boolean isValidPassword(String password) {
-        String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,}$";
-        Pattern pattern = Pattern.compile(regex);
+        Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
         Matcher matcher = pattern.matcher(password);
         return matcher.matches();
     }
