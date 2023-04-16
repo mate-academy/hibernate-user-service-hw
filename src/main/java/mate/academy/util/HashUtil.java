6445a5a858de38c393ad68225242b7da3b34df85
Ilko Good <ilko.engineering@gmail.com>
2023-04-16 10:23:23 +0300

    User services implemented

---
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -0,0 +1,35 @@
+package mate.academy.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class HashUtil {
+    private static final String ALGORITHM = "SHA-256";
+    private static final SecureRandom secureRandom = new SecureRandom();
+
+    private HashUtil() {
+    }
+
+    public static byte[] getSalt() {
+        byte[] salt = new byte[16];
+        secureRandom.nextBytes(salt);
+        return salt;
+    }
+
+    public static String hashPassword(String password, byte[] salt) {
+        StringBuilder hashedPassword = new StringBuilder();
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance(ALGORITHM);
+            messageDigest.update(salt);
+            //messageDigest.update(salt);
+            byte[] digest = messageDigest.digest(password.getBytes());
+            for (byte b : digest) {
+                hashedPassword.append(String.format("%02x", b));
+            }
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException("Couldn't create hash using " + ALGORITHM, e);
+        }
+        return hashedPassword.toString();
+    }
+}
