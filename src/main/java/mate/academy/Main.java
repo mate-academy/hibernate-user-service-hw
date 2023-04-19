b5197c5e5fb5131ded9328a19425397fd6bf52fb
Oleksii Stepaniak <alexstepanyak@gmail.com>
2023-04-19 18:10:32 +0300

    first version of hibernate user service hw

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,55 +1,41 @@
 package mate.academy;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import mate.academy.model.CinemaHall;
-import mate.academy.model.Movie;
-import mate.academy.model.MovieSession;
-import mate.academy.service.CinemaHallService;
-import mate.academy.service.MovieService;
-import mate.academy.service.MovieSessionService;
+import mate.academy.lib.Injector;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
 
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final AuthenticationService authenticationService
+            = (AuthenticationService) injector.getInstance(AuthenticationService.class);
+
     public static void main(String[] args) {
-        MovieService movieService = null;
-
-        Movie fastAndFurious = new Movie("Fast and Furious");
-        fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
-        movieService.add(fastAndFurious);
-        System.out.println(movieService.get(fastAndFurious.getId()));
-        movieService.getAll().forEach(System.out::println);
-
-        CinemaHall firstCinemaHall = new CinemaHall();
-        firstCinemaHall.setCapacity(100);
-        firstCinemaHall.setDescription("first hall with capacity 100");
-
-        CinemaHall secondCinemaHall = new CinemaHall();
-        secondCinemaHall.setCapacity(200);
-        secondCinemaHall.setDescription("second hall with capacity 200");
-
-        CinemaHallService cinemaHallService = null;
-        cinemaHallService.add(firstCinemaHall);
-        cinemaHallService.add(secondCinemaHall);
-
-        System.out.println(cinemaHallService.getAll());
-        System.out.println(cinemaHallService.get(firstCinemaHall.getId()));
-
-        MovieSession tomorrowMovieSession = new MovieSession();
-        tomorrowMovieSession.setCinemaHall(firstCinemaHall);
-        tomorrowMovieSession.setMovie(fastAndFurious);
-        tomorrowMovieSession.setShowTime(LocalDateTime.now().plusDays(1L));
-
-        MovieSession yesterdayMovieSession = new MovieSession();
-        yesterdayMovieSession.setCinemaHall(firstCinemaHall);
-        yesterdayMovieSession.setMovie(fastAndFurious);
-        yesterdayMovieSession.setShowTime(LocalDateTime.now().minusDays(1L));
-
-        MovieSessionService movieSessionService = null;
-        movieSessionService.add(tomorrowMovieSession);
-        movieSessionService.add(yesterdayMovieSession);
-
-        System.out.println(movieSessionService.get(yesterdayMovieSession.getId()));
-        System.out.println(movieSessionService.findAvailableSessions(
-                        fastAndFurious.getId(), LocalDate.now()));
+        try {
+            User alexUser = authenticationService
+                    .register("alexemail@gmail.com", "qwerty");
+            User felixUser = authenticationService
+                    .register("felixemail@gmail.com", "helloWorld");
+            User aliceUser = authenticationService
+                    .register("aliceemail@gmail.com", "ukraine1991");
+            // AN ADDITIONAL MESSAGE
+            System.out.println("I have registered new instances of users");
+        } catch (Exception e) {
+            throw new RuntimeException("Couldn't register users", e);
+        }
+        try {
+            User authenticatedAlexUser = authenticationService
+                    .login("alexemail@gmail.com", "qwerty");
+            User authenticatedFelixUser = authenticationService
+                    .login("felixemail@gmail.com", "helloWorld");
+            User authenticatedAliceUser = authenticationService
+                    .login("aliceemail@gmail.com", "ukraine1991");
+            System.out.println(authenticatedAlexUser);
+            System.out.println(authenticatedFelixUser);
+            System.out.println(authenticatedAliceUser);
+            // AN ADDITIONAL MESSAGE
+            System.out.println("I have printed some authenticated users");
+        } catch (Exception e) {
+            throw new RuntimeException("Couldn't authorize users", e);
+        }
     }
 }
