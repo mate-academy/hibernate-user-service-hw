6845c233e9d5c34596b2cd319b589642be786239
Oleh Sarapuk <olehsarapuk@gmail.com>
2023-04-20 08:42:11 +0300

    refactored exceptions

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,5 +1,7 @@
 package mate.academy;
 
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
 import mate.academy.lib.Injector;
 import mate.academy.model.User;
 import mate.academy.service.AuthenticationService;
@@ -9,7 +11,7 @@ public class Main {
     private static AuthenticationService authenticationService =
             (AuthenticationService) injector.getInstance(AuthenticationService.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws AuthenticationException, RegistrationException {
         User register = authenticationService.register("email", "password");
         System.out.println(register);
         User login = authenticationService.login("email", "password");
