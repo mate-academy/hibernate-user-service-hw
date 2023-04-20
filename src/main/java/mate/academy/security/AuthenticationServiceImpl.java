2ff1d4e17c441ecde3e8d6652a47203b3e5c584e
Maksym <maxim.yashyn@gmail.com>
2023-04-20 12:46:31 +0300

    implemented task

---
+++ src/main/java/mate/academy/security/AuthenticationServiceImpl.java
@@ -0,0 +1,39 @@
+package mate.academy.security;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> userOptional = userService.findByEmail(email);
+        User user = null;
+        String hashedPassword = null;
+        if (userOptional.isPresent()) {
+            user = userOptional.get();
+            hashedPassword = HashUtil.hashPassword(password, user.getSalt());
+        }
+        if (userOptional.isEmpty() || !user.getPassword().equals(hashedPassword)) {
+            throw new AuthenticationException("Can't authenticate User by email: " + email);
+        }
+        return user;
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        if (userService.findByEmail(email).isPresent()) {
+            throw new RegistrationException("Can't register user. Incorrect email or password");
+        }
+        return userService.add(new User(email, password));
+    }
+}
