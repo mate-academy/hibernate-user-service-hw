80aed299537f0e86fde2d1f73652b4b55ed40220
Elvira Solnyshkina <elvira.solnyshkina@gmail.com>
2023-04-20 09:11:59 +0300

    impl user service hibernate

---
+++ src/main/java/mate/academy/exception/AuthenticationException.java
@@ -0,0 +1,12 @@
+package mate.academy.exception;
+
+public class AuthenticationException extends Exception {
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AuthenticationException(String message) {
+        super(message);
+
+    }
+}
