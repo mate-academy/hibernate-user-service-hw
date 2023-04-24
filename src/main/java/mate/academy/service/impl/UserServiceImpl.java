98f18e4bedc7fbf3f5772c8708249414bfeae79c
Vitalii Romanyuk <vitaliy37777@gmail.com>
2023-04-24 19:11:43 +0300

    in process

---
+++ src/main/java/mate/academy/service/impl/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package mate.academy.service.impl;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import mate.academy.dao.UserDao;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Inject
+    private UserDao userDao;
+
+    @Override
+    public User add(User user) {
+        return userDao.add(user);
+    }
+
+    @Override
+    public User get(Long id) {
+        return userDao.get(id).orElseThrow(() ->
+                new NoSuchElementException("Can't get user by id: " + id)
+        );
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return userDao.findByEmail(email);
+    }
+}
