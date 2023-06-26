9601c86f96f170ca57a51e794b24c97fd29c2510
pavlokhomiak <pavlo.khomiak93@gmail.com>
2023-06-26 12:50:50 +0300

    solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -2,7 +2,6 @@ package mate.academy;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-
 import mate.academy.exception.AuthenticationException;
 import mate.academy.exception.RegistrationException;
 import mate.academy.lib.Injector;
@@ -19,7 +18,7 @@ public class Main {
 
     private static final Injector injector = Injector.getInstance("mate.academy");
 
-    public static void main (String[]args) throws RegistrationException, AuthenticationException {
+    public static void main(String[]args) throws RegistrationException, AuthenticationException {
         MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
 
         Movie fastAndFurious = new Movie("Fast and Furious");
