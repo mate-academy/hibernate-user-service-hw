2cee3981444759bb4c31b3379c32e8653b8cd810
Balan Alex <litecloud.nohax@gmail.com>
2023-07-11 10:37:30 +0300

    fixed checkstyle issues

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -15,68 +15,68 @@ import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
 
 public class Main {
-        private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final Injector injector = Injector.getInstance("mate.academy");
 
-        public static void main(String[] args) throws RegistrationException, AuthenticationException {
-            MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+    public static void main(String[] args) throws RegistrationException, AuthenticationException {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
 
-            Movie fastAndFurious = new Movie("Fast and Furious");
-            fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
-            movieService.add(fastAndFurious);
-            System.out.println(movieService.get(fastAndFurious.getId()));
-            movieService.getAll().forEach(System.out::println);
-            CinemaHall firstCinemaHall = new CinemaHall();
-            firstCinemaHall.setCapacity(100);
-            firstCinemaHall.setDescription("first hall with capacity 100");
-            CinemaHall secondCinemaHall = new CinemaHall();
-            secondCinemaHall.setCapacity(200);
-            secondCinemaHall.setDescription("second hall with capacity 200");
+        Movie fastAndFurious = new Movie("Fast and Furious");
+        fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
+        movieService.add(fastAndFurious);
+        System.out.println(movieService.get(fastAndFurious.getId()));
+        movieService.getAll().forEach(System.out::println);
+        CinemaHall firstCinemaHall = new CinemaHall();
+        firstCinemaHall.setCapacity(100);
+        firstCinemaHall.setDescription("first hall with capacity 100");
+        CinemaHall secondCinemaHall = new CinemaHall();
+        secondCinemaHall.setCapacity(200);
+        secondCinemaHall.setDescription("second hall with capacity 200");
 
-            CinemaHallService cinemaHallService = (CinemaHallService)
-                    injector.getInstance(CinemaHallService.class);
-            cinemaHallService.add(firstCinemaHall);
-            cinemaHallService.add(secondCinemaHall);
+        CinemaHallService cinemaHallService = (CinemaHallService)
+                injector.getInstance(CinemaHallService.class);
+        cinemaHallService.add(firstCinemaHall);
+        cinemaHallService.add(secondCinemaHall);
 
-            System.out.println(cinemaHallService.getAll());
-            System.out.println(cinemaHallService.get(firstCinemaHall.getId()));
-            MovieSession tomorrowMovieSession = new MovieSession();
-            tomorrowMovieSession.setCinemaHall(firstCinemaHall);
-            tomorrowMovieSession.setMovie(fastAndFurious);
-            tomorrowMovieSession.setShowTime(LocalDateTime.now().plusDays(1L));
-            MovieSession yesterdayMovieSession = new MovieSession();
-            yesterdayMovieSession.setCinemaHall(firstCinemaHall);
-            yesterdayMovieSession.setMovie(fastAndFurious);
-            yesterdayMovieSession.setShowTime(LocalDateTime.now().minusDays(1L));
+        System.out.println(cinemaHallService.getAll());
+        System.out.println(cinemaHallService.get(firstCinemaHall.getId()));
+        MovieSession tomorrowMovieSession = new MovieSession();
+        tomorrowMovieSession.setCinemaHall(firstCinemaHall);
+        tomorrowMovieSession.setMovie(fastAndFurious);
+        tomorrowMovieSession.setShowTime(LocalDateTime.now().plusDays(1L));
+        MovieSession yesterdayMovieSession = new MovieSession();
+        yesterdayMovieSession.setCinemaHall(firstCinemaHall);
+        yesterdayMovieSession.setMovie(fastAndFurious);
+        yesterdayMovieSession.setShowTime(LocalDateTime.now().minusDays(1L));
 
-            MovieSessionService movieSessionService = (MovieSessionService)
-                    injector.getInstance(MovieSessionService.class);
-            movieSessionService.add(tomorrowMovieSession);
-            movieSessionService.add(yesterdayMovieSession);
+        MovieSessionService movieSessionService = (MovieSessionService)
+                injector.getInstance(MovieSessionService.class);
+        movieSessionService.add(tomorrowMovieSession);
+        movieSessionService.add(yesterdayMovieSession);
 
-            System.out.println(movieSessionService.get(yesterdayMovieSession.getId()));
-            System.out.println(movieSessionService.findAvailableSessions(
-            fastAndFurious.getId(), LocalDate.now()));
-            System.out.println("*** *** ***");
-            String bobEmail = "bob777@gmail.com";
-            String bobPassword = "qwerty123";
-            String aliceEmail = "alice777@gmail.com";
-            String alicePassword = "qwerty456";
-            String johnEmail = "john777@gmail.com";
-            String johnPassword = "qwerty789";
+        System.out.println(movieSessionService.get(yesterdayMovieSession.getId()));
+        System.out.println(movieSessionService.findAvailableSessions(
+                fastAndFurious.getId(), LocalDate.now()));
+        System.out.println("*** *** ***");
+        String bobEmail = "bob777@gmail.com";
+        String bobPassword = "qwerty123";
+        String aliceEmail = "alice777@gmail.com";
+        String alicePassword = "qwerty456";
+        String johnEmail = "john777@gmail.com";
+        String johnPassword = "qwerty789";
 
-            AuthenticationService authenticationService = (AuthenticationService)
-                    injector.getInstance(AuthenticationService.class);
-            System.out.println(authenticationService.register(bobEmail, bobPassword));
-            System.out.println(authenticationService.register(aliceEmail, alicePassword));
-            System.out.println(authenticationService.register(johnEmail, johnPassword));
+        AuthenticationService authenticationService = (AuthenticationService)
+                injector.getInstance(AuthenticationService.class);
+        System.out.println(authenticationService.register(bobEmail, bobPassword));
+        System.out.println(authenticationService.register(aliceEmail, alicePassword));
+        System.out.println(authenticationService.register(johnEmail, johnPassword));
 
-            User bob = authenticationService.login(bobEmail, bobPassword);
-            User alice = authenticationService.login(aliceEmail, alicePassword);
-            User john = authenticationService.login(johnEmail, johnPassword);
-            User[] users = {bob, alice, john};
-            for (User user : users) {
-                System.out.println("User's email from db: " + user.getEmail()
-                        + " Hashed password " + user.getPassword());
-            }
+        User bob = authenticationService.login(bobEmail, bobPassword);
+        User alice = authenticationService.login(aliceEmail, alicePassword);
+        User john = authenticationService.login(johnEmail, johnPassword);
+        User[] users = {bob, alice, john};
+        for (User user : users) {
+            System.out.println("User's email from db: " + user.getEmail()
+                    + " Hashed password " + user.getPassword());
         }
     }
+}
