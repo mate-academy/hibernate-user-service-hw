37320173b4a03c2864f406a012b53523b9061194
Yaroslav Liashchenko <liaroswin@gmail.com>
2023-08-01 14:41:11 +0300

    refactored login method in AuthenticationServiceImpl and hashPassword in HashUtil

--- src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -18,14 +18,12 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public User login(String email, String password) throws AuthenticationException {
         Optional<User> userFromDbOptional = userService.findByEmail(email);
-        if (userFromDbOptional.isEmpty()) {
-            throw new AuthenticationException("Can`t authenticate user with email " + email
-                    + ". Email or password was incorrect.");
-        }
-        User user = userFromDbOptional.get();
-        String hashedPassword = HashUtil.hashPassword(password, user.getSalt());
-        if (user.getPassword().equals(hashedPassword)) {
-            return user;
+        if (!userFromDbOptional.isEmpty()) {
+            User user = userFromDbOptional.get();
+            String hashedPassword = HashUtil.hashPassword(password, user.getSalt());
+            if (user.getPassword().equals(hashedPassword)) {
+                return user;
+            }
         }
         throw new AuthenticationException("Can`t authenticate user with email " + email
                 + ". Email or password was incorrect.");
