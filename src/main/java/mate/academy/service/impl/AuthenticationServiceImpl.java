4ae06b4db38db3aac030bcd34f209a0d2b309185
literandltx <romanskovronn@gmail.com>
2023-08-19 16:29:55 +0300

    added AuthenticationService

--- src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -6,6 +6,9 @@ import mate.academy.lib.Inject;
 import mate.academy.model.User;
 import mate.academy.service.AuthenticationService;
 import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+import java.util.Optional;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
     @Inject
@@ -13,11 +16,32 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public User login(String email, String password) throws AuthenticationException {
-        return null;
+        Optional<User> byEmail = userService.findByEmail(email);
+
+        if (byEmail.isEmpty()) {
+            throw new AuthenticationException("Could not authenticate user by email:" + email);
+        }
+
+        User user = byEmail.get();
+        String hashedPassword = HashUtil.hashPassword(password, user.getSalt());
+        if (hashedPassword.equals(user.getPassword())) {
+            return user;
+        }
+
+        throw new AuthenticationException("Could not authenticate user by email:" + email);
     }
 
     @Override
     public User register(String email, String password) throws RegistrationException {
-        return null;
+        Optional<User> byEmail = userService.findByEmail(email);
+
+        if (byEmail.isPresent()) {
+            throw new RegistrationException("This email address is already registered. Email: " + email);
+        }
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+
+        return userService.add(user);
     }
 }
