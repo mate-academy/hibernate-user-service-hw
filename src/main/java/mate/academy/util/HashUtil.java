2119b8194b17567f12fa02e347aa9cbe5c95e039
Zhdanov Anton <anton1992xxxx@gmail.com>
2023-08-19 21:05:55 +0300

    hw-hibernate-user-service

---
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -0,0 +1,34 @@
+package mate.academy.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class HashUtil {
+    private static final String HASH_ALGORITHM = "SHA-256";
+    private static final String STRING_FORMAT = "%02x";
+
+    public HashUtil() {
+    }
+
+    public static byte[] getSalt() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] salt = new byte[15];
+        secureRandom.nextBytes(salt);
+        return salt;
+    }
+
+    public static String hashPassword(String password, byte[] salt) {
+        StringBuilder hashBuilder = new StringBuilder();
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance(HASH_ALGORITHM);
+            byte[] digest = messageDigest.digest(password.getBytes());
+            for (byte b : digest) {
+                hashBuilder.append(String.format(STRING_FORMAT, b));
+            }
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException("Can't create using hash", e);
+        }
+        return hashBuilder.toString();
+    }
+}
