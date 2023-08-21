c36eaa44beadf259fe62b63169dfae905d0e7339
Vladyslav Ihnatiuk <123470878+Someboty@users.noreply.github.com>
2023-08-21 17:34:03 +0300

    Implemented User model, UserDao, UserService and AuthenticationService with implementations, RegistrationException and AuthenticationException

---
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,43 @@
+package mate.academy.service.impl;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> userOptional = userService.findByEmail(email);
+        if (userOptional.isEmpty() || !checkPassword(userOptional.get(), password)) {
+            throw new AuthenticationException("Can't authenticate user with email: " + email);
+        }
+        return userOptional.get();
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        if (email.isEmpty() || password.isEmpty()) {
+            throw new RegistrationException(
+                    "Can't register a user with empty email or password, email: " + email);
+        }
+        if (userService.findByEmail(email).isPresent()) {
+            throw new RegistrationException("User with same email already exists: " + email);
+        }
+        User user = new User(email, password);
+        return userService.add(user);
+    }
+
+    private boolean checkPassword(User user, String password) {
+        return user.getPassword().equals(HashUtil.hashPassword(password, user.getSalt()));
+    }
+}
