c26cfb05cfce48aaedbcfb8bad4e8a070d949252
Zhdanov Anton <anton1992xxxx@gmail.com>
2023-08-24 12:15:07 +0300

    add improvments

--- src/main/java/mate/academy/util/HashUtil.java
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -7,13 +7,14 @@ import java.security.SecureRandom;
 public class HashUtil {
     private static final String HASH_ALGORITHM = "SHA-256";
     private static final String STRING_FORMAT = "%02x";
+    private static final int DEFAULT_SALT_CAPACITY = 15;
 
     public HashUtil() {
     }
 
     public static byte[] getSalt() {
         SecureRandom secureRandom = new SecureRandom();
-        byte[] salt = new byte[15];
+        byte[] salt = new byte[DEFAULT_SALT_CAPACITY];
         secureRandom.nextBytes(salt);
         return salt;
     }
@@ -22,6 +23,7 @@ public class HashUtil {
         StringBuilder hashBuilder = new StringBuilder();
         try {
             MessageDigest messageDigest = MessageDigest.getInstance(HASH_ALGORITHM);
+            messageDigest.update(salt);
             byte[] digest = messageDigest.digest(password.getBytes());
             for (byte b : digest) {
                 hashBuilder.append(String.format(STRING_FORMAT, b));
