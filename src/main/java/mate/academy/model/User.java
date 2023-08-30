9b278383350daf07d9a07908cd73483f03ae29cb
silchenko-arsen <silchenkoarsen@gmail.com>
2023-08-30 15:00:29 +0300

    do exercise

---
+++ src/main/java/mate/academy/model/User.java
@@ -0,0 +1,82 @@
+package mate.academy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.Arrays;
+import java.util.Objects;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private byte[] salt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public byte[] getSalt() {
+        return salt;
+    }
+
+    public void setSalt(byte[] salt) {
+        this.salt = salt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof User user)) {
+            return false;
+        }
+        return Objects.equals(getId(), user.getId()) && Objects.equals(getEmail(),
+                user.getEmail()) && Objects.equals(getPassword(),
+                user.getPassword()) && Arrays.equals(getSalt(), user.getSalt());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(getId(), getEmail(), getPassword());
+        result = 31 * result + Arrays.hashCode(getSalt());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "id=" + id
+                + ", email='" + email + '\''
+                + ", password='" + password + '\''
+                + ", salt=" + Arrays.toString(salt)
+                + '}';
+    }
+}
---
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,45 @@
+package mate.academy.service.impl;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> userOptional = userService.findByEmail(email);
+        if (userOptional.isEmpty() || !isPasswordValid(userOptional.get(), password)) {
+            throw new AuthenticationException(
+                    "Can't authenticate user with this password: " + password);
+        }
+        return userOptional.get();
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        Optional<User> userOptional = userService.findByEmail(email);
+        if (userOptional.isPresent()) {
+            throw new RegistrationException(
+                    "Can not register new user. User with email " + email + " already exists");
+        }
+        User uregisteredUser = new User();
+        uregisteredUser.setEmail(email);
+        uregisteredUser.setPassword(password);
+        return uregisteredUser;
+    }
+
+    private boolean isPasswordValid(User user, String password) {
+        String hashedPassword = HashUtil.hashPassword(user.getPassword(), user.getSalt());
+        return hashedPassword.equals(password);
+    }
+}
