b18e5d45b1ab722dac7a5aecf77fa97242dbc6dd
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-09-17 14:42:06 +0300

    added User model

---
+++ src/main/java/mate/academy/model/User.java
@@ -0,0 +1,50 @@
+package mate.academy.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String login;
+    private String password;
+    private Byte[] salt = new Byte[16];
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Byte[] getSalt() {
+        return salt;
+    }
+
+    public void setSalt(Byte[] salt) {
+        this.salt = salt;
+    }
+}
