321c88ac486eb3c9dd1486d4bfa4b6b17c14a528
yuriistorozhuk <yurii.storozhuk@gmail.com>
2023-09-29 08:36:00 +0300

    fix<HashUtil>: make final and private constructor

--- src/main/java/mate/academy/util/HashUtil.java
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -4,10 +4,10 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
-public class HashUtil {
+public final class HashUtil {
     private static final String CRYPTO_ALGORITHM = "SHA-512";
 
-    public HashUtil() {
+    private HashUtil() {
     }
 
     public static byte[] getSalt() {
@@ -27,7 +27,8 @@ public class HashUtil {
                 hashedPassword.append(String.format("%02x", b));
             }
         } catch (NoSuchAlgorithmException e) {
-            throw new IllegalStateException("Could not create hash using SHA-512 algorithm", e);
+            throw new IllegalStateException("Could not create hash using SHA-512 algorithm"
+                    + CRYPTO_ALGORITHM, e);
         }
         return hashedPassword.toString();
     }
