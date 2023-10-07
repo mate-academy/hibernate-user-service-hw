332eb86c4aa0552b9be20d63e4f2ade23ea68763
Oleh <popovich158@gmail.com>
2023-10-08 00:54:52 +0300

    Created User model,UserDao,UserService,AuthenticationService and checked exceptions.

---
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -0,0 +1,31 @@
+package mate.academy.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class HashUtil {
+    private static final String CRYPTO_ALGORITHM = "SHA-512";
+
+    public static byte[] getSalt() {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        return salt;
+    }
+
+    public static String hashPassword(String password, byte[] salt) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance(CRYPTO_ALGORITHM);
+            messageDigest.update(salt);
+            byte[] digest = messageDigest.digest(password.getBytes());
+            for (byte b : digest) {
+                stringBuilder.append(String.format("%02x", b));
+            }
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException("Couldn't create hash using SHA-512 algorithm");
+        }
+        return stringBuilder.toString();
+    }
+}
