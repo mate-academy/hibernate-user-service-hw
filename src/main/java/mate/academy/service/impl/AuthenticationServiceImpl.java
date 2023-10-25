b577a0f39865a9ae1fa997fd900bcf3388be1d9b
Anatolii Nazarov <anatoly_n@ukr.net>
2023-10-25 16:46:00 +0300

    Fixed after remarks

--- src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -18,15 +18,12 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public User login(String email, String password) throws AuthenticationException {
         Optional<User> userFromDbOptional = userService.findByEmail(email);
-        if (userFromDbOptional.isEmpty()) {
-            throw new AuthenticationException("Can't authenticate user");
-        }
         User user = userFromDbOptional.get();
         String hashedPassword = HashUtil.hashPassword(password, user.getSalt());
-        if (user.getPasssword().equals(hashedPassword)) {
+        if (userFromDbOptional.isPresent() && user.getPasssword().equals(hashedPassword)) {
             return user;
         }
-        throw new AuthenticationException("Can't authenticate user");
+        throw new AuthenticationException("Username or password not found");
     }
 
     @Override
