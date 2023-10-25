635ebd396755435618570fb24486bcf5e4c9dec1
Anatolii Nazarov <anatoly_n@ukr.net>
2023-10-25 13:09:38 +0300

    For check

---
+++ src/main/java/mate/academy/model/User.java
@@ -0,0 +1,65 @@
+package mate.academy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private byte[] salt;
+
+    public User() {
+    }
+
+    public User(String password, String email) {
+    }
+
+    public byte[] getSalt() {
+        return salt;
+    }
+
+    public void setSalt(byte[] salt) {
+        this.salt = salt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPasssword() {
+        return password;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPasssword(String passsword) {
+        this.password = passsword;
+    }
+
+    @Override
+    public String toString() {
+        return "mate.academy.model.User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", passsword='" + password + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/mate/academy/service/impl/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package mate.academy.service.impl;
+
+import mate.academy.dao.UserDao;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.UserService;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Inject
+    private UserDao userDao;
+
+    @Override
+    public User add(User user) {
+        return userDao.add(user);
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return userDao.findByEmail(email);
+    }
+}
