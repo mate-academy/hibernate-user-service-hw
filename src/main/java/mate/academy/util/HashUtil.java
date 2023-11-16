345c4b556a31dffbef3731f100da9be26fe1466a
Dmytro Zarudnyi <dmytro.zarudnyi@gmail.com>
2023-11-16 12:49:29 +0200

    finished hw

---
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -0,0 +1,37 @@
+package mate.academy.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class HashUtil {
+    private static final String CRYPTO_ALGORITHM = "SHA-512";
+
+    private HashUtil() {
+
+    }
+
+    public static byte[] getSalt() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] salt = new byte[16];
+        secureRandom.nextBytes(salt);
+        return salt;
+    }
+
+    public static String hashPassword(String password, byte[] salt) {
+        StringBuilder hashedPas = new StringBuilder();
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance(CRYPTO_ALGORITHM);
+            messageDigest.update(salt);
+            byte[] digest = messageDigest.digest(password.getBytes());
+            for (byte b : digest) {
+                hashedPas.append(String.format("%02x", b));
+            }
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(
+                    "Was not able to create hashed password with SHA-512 algorithm", e);
+        }
+
+        return hashedPas.toString();
+    }
+}
