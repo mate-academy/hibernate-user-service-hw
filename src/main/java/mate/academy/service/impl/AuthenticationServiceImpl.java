7ba100f1d8a3317b2bbdb54bb871e07545f2e26d
MateuszRostek <17rostek17@gmail.com>
2023-12-29 19:23:58 +0100

    Everything implemented

--- src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -1,8 +1,43 @@
 package mate.academy.service.impl;
 
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
+import mate.academy.model.User;
 import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> optionalUser = userService.findByEmail(email);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            String hashedPassword = HashUtil.hashPassword(password, user.getSalt());
+            if (user.getPassword().equals(hashedPassword)) {
+                return user;
+            }
+        }
+        throw new AuthenticationException("Can't log in to app! Incorrect email/password");
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        Optional<User> optionalUser = userService.findByEmail(email);
+        if (optionalUser.isPresent()) {
+            throw new RegistrationException(
+                    "Can't register new user! Email: " + email + " is already taken!");
+        }
+        User userToRegister = new User();
+        userToRegister.setEmail(email);
+        userToRegister.setPassword(password);
+        return userService.add(userToRegister);
+    }
 }
