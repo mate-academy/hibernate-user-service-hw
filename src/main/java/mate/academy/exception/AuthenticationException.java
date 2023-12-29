fd338b0448b1ab78286d4afcbed2113859668d95
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-29 20:15:37 +0200

    added changes to the class AuthenticationException

--- src/main/java/mate/academy/exception/AuthenticationException.java
+++ src/main/java/mate/academy/exception/AuthenticationException.java
@@ -1,2 +1,11 @@
-package mate.academy.exception;public class AuthenticationException {
+package mate.academy.exception;
+
+public class AuthenticationException extends Exception {
+    public AuthenticationException(String message, Throwable ex) {
+        super(message, ex);
+    }
+
+    public AuthenticationException(String message) {
+        super(message);
+    }
 }
