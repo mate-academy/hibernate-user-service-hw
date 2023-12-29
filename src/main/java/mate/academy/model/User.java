fb6ab3da2e5d4a9f7614f418dbd5d4caa9946875
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-29 21:23:00 +0200

    added changes to the class User

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -60,10 +60,10 @@ public class User {
 
     @Override
     public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
+        return "User{"
+                + "id=" + id
+                + ", email='" + email + '\''
+                + ", password='" + password + '\''
+                + '}';
     }
 }
