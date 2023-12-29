2773d67d23233f7d75cf1f49c90c9d068f1c45a9
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-29 20:14:44 +0200

    added changes to the class RegistrationException

--- src/main/java/mate/academy/exception/RegistrationException.java
+++ src/main/java/mate/academy/exception/RegistrationException.java
@@ -1,2 +1,11 @@
-package mate.academy.exception;public class RegistrationException {
+package mate.academy.exception;
+
+public class RegistrationException extends Exception {
+    public RegistrationException(String message) {
+        super(message);
+    }
+
+    public RegistrationException(String message, Throwable e) {
+        super(message, e);
+    }
 }
