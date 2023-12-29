930cbd458d4ccf912432c4ba0241a42833fd4078
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-29 20:38:30 +0200

    added changes to the class AuthenticationServiceImpl

--- src/main/java/mate/academy/security/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/security/AuthenticationServiceImpl.java
@@ -1,2 +1,44 @@
-package mate.academy.security;public class AuthenticationServiceImpl {
+package mate.academy.security;
+
+import javax.naming.AuthenticationException;
+import java.util.Optional;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        Optional<User> userFromDBOptional = userService.findByEmail(email);
+        if (userFromDBOptional.isEmpty()) {
+            throw new RegistrationException("Can`t registrate user");
+        }
+        User user = userFromDBOptional.get();
+        String hashedPassword = HashUtil.hashPassword(password, user.getSalt());
+        if (user.getPassword().equals(hashedPassword)) {
+            return user;
+        }
+        throw new RegistrationException("Can`t registrate user");
+    }
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> userFromDBOptional = userService.findByEmail(email);
+        if (userFromDBOptional.isEmpty()) {
+            throw new AuthenticationException("Can`t authenticate user");
+        }
+        User user = userFromDBOptional.get();
+        String hashedPassword = HashUtil.hashPassword(password, user.getSalt());
+        if (user.getPassword().equals(hashedPassword)) {
+            return user;
+        }
+        throw new AuthenticationException("Can`t authenticate user");
+    }
 }
