76547df8ed93f93e1a9ff8dbb924c199613698b1
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-29 21:25:49 +0200

    added changes to the class HashUtil

--- src/main/java/mate/academy/util/HashUtil.java
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -1,9 +1,9 @@
 package mate.academy.util;
 
-import mate.academy.exception.DataProcessingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import mate.academy.exception.DataProcessingException;
 
 public class HashUtil {
     private static final String CRYPTO_ALGORITHM = "SHA-512";
