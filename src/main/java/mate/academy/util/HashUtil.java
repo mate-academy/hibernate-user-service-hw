9080c3077133599d22674eea3c6ba2b5c4c5e997
MateuszRostek <17rostek17@gmail.com>
2023-12-29 16:52:32 +0100

    HashUtil, UserDao and User implemented

--- src/main/java/mate/academy/util/HashUtil.java
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -1,4 +1,37 @@
 package mate.academy.util;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import mate.academy.exception.DataProcessingException;
+
 public class HashUtil {
+    private static final String CRYPTO_ALGORITHM = "SHA-512";
+    private static final int BYTE_ARRAY_SIZE = 16;
+
+    private HashUtil() {
+    }
+
+    public static byte[] getSalt() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] salt = new byte[BYTE_ARRAY_SIZE];
+        secureRandom.nextBytes(salt);
+        return salt;
+    }
+
+    public static String hashPassword(String password, byte[] salt) {
+        StringBuilder hashedPassword = new StringBuilder();
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance(CRYPTO_ALGORITHM);
+            messageDigest.update(salt);
+            byte[] digest = messageDigest.digest(password.getBytes());
+            for (byte singleByte : digest) {
+                hashedPassword.append(String.format("%02x", singleByte));
+            }
+        } catch (NoSuchAlgorithmException e) {
+            throw new DataProcessingException(
+                    "Can't create hash using " + CRYPTO_ALGORITHM + " algorithm", e);
+        }
+        return hashedPassword.toString();
+    }
 }
