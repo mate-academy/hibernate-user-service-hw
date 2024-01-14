dae484248af4af08999bee031621809551384b9f
Piotr Kaliszuk <p_kaliszuk@tlen.pl>
2024-01-14 14:52:21 +0100

    register method works, but the other one does not, i think it's because of hashing i can't compare them properly, they change dynamically i don't know how to fix it, need help

---
+++ src/main/java/mate/academy/security/AuthenticationServiceImpl.java
@@ -0,0 +1,58 @@
+package mate.academy.security;
+
+import java.util.Optional;
+import javax.security.sasl.AuthenticationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+
+        Optional<User> userByEmail = userService.findByEmail(email);
+
+        if (userByEmail.isPresent()) {
+            User user = userByEmail.get();
+            String hashedPassword = HashUtil.hashPassword(password, user.getSalt());
+
+            if (user.getPassword().equals(hashedPassword)) {
+                return user;
+            }
+        }
+
+        throw new AuthenticationException("Incorrect email or password");
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        Optional<User> userByEmail = userService.findByEmail(email);
+        if (userByEmail.isPresent()) {
+            throw new RegistrationException("User with this email exist");
+        }
+
+        User user = new User();
+        user.setEmail(email);
+
+        byte[] salt = HashUtil.getSalt();
+        String hashedPassword = HashUtil.hashPassword(password, salt);
+
+        user.setSalt(salt);
+        user.setPassword(hashedPassword);
+
+        User registeredUser = userService.add(user);
+
+        if (registeredUser == null) {
+            throw new RegistrationException("Failed to register user");
+        }
+
+        return registeredUser;
+    }
+}
