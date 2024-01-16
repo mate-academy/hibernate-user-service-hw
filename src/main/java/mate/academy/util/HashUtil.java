d0dba801c663ee05d300b3b3f6c4528d0286e233
TkachukTara <zaz42988@gmail.com>
2024-01-16 10:25:16 +0200

    Done User-Service HW! V2

--- src/main/java/mate/academy/util/HashUtil.java
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -6,12 +6,12 @@ import java.security.SecureRandom;
 
 public class HashUtil {
     private static final String CRYPTO_ALGORITHM = "SHA-512";
+    private static final SecureRandom secureRandom = new SecureRandom();
 
     private HashUtil() {
     }
 
     public static byte[] getSalt() {
-        SecureRandom secureRandom = new SecureRandom();
         byte[] salt = new byte[16];
         secureRandom.nextBytes(salt);
         return salt;
@@ -26,9 +26,9 @@ public class HashUtil {
             for (byte b: digest) {
                 hashedPassword.append(String.format("%02x", b));
             }
+            return hashedPassword.toString();
         } catch (NoSuchAlgorithmException e) {
             throw new IllegalStateException("Could not create hash using SHA-512 algorithm", e);
         }
-        return hashedPassword.toString();
     }
 }
