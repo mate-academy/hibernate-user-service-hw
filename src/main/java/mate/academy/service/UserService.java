ec8f79e3f4e42b9fb761b2e5bdf5ee4cca2d860b
Olga Starkova <ole4kastar79@gmail.com>
2024-01-25 00:01:39 +0200

    hibernate-user-service_by_Starikova

---
+++ src/main/java/mate/academy/service/UserService.java
@@ -0,0 +1,9 @@
+package mate.academy.service;
+
+import java.util.Optional;
+import mate.academy.model.User;
+
+public interface UserService {
+    User save(User user);
+    Optional<User> findByEmail(String email);
+}
