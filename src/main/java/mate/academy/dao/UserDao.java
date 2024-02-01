259b1dfec6026b2fe03e739bcf867cefc6d68819
Anna Suprun <evgenevnaanna989@gmail.com>
2024-02-02 01:24:29 +0200

    created User, dao, userService, AuthenticationService, exceptions

---
+++ src/main/java/mate/academy/dao/UserDao.java
@@ -0,0 +1,15 @@
+package mate.academy.dao;
+
+import java.util.List;
+import java.util.Optional;
+import mate.academy.model.User;
+
+public interface UserDao {
+    User add(User movie);
+
+    Optional<User> get(Long id);
+
+    List<User> getAll();
+
+    Optional<User> getByEmail(String email);
+}
