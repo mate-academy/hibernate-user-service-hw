af5bbfbcfebf939b54c9094b0b1f9d940220d847
Bohdan Bilko <rapsbodik@gmail.com>
2024-02-13 21:53:17 +0200

    first solution

--- src/main/java/mate/academy/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/service/impl/MovieServiceImpl.java
@@ -12,6 +12,9 @@ public class MovieServiceImpl implements MovieService {
     @Inject
     private MovieDao movieDao;
 
+    public MovieServiceImpl() {
+    }
+
     @Override
     public Movie add(Movie movie) {
         return movieDao.add(movie);
