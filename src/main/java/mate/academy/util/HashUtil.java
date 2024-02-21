9f4a37f58da06d33f91e596705156745e555120e
Kovaev <eugenkoval1010@gmail.com>
2024-02-21 10:03:24 +0200

    fixed variable initialization

--- src/main/java/mate/academy/util/HashUtil.java
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -6,12 +6,12 @@ import java.security.SecureRandom;
 
 public class HashUtil {
     private static final String CRYPTO_ALGORITHM = "SHA-512";
+    private static final SecureRandom secureRandom = new SecureRandom();
 
     private HashUtil() {
     }
 
     public static byte[] getSalt() {
-        SecureRandom secureRandom = new SecureRandom();
         byte[] salt = new byte[16];
         secureRandom.nextBytes(salt);
         return salt;
