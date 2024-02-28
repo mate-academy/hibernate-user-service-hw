fdc9c116135d5c3404549815346fdb980e090c61
AdamMudrak <mudrets2000@gmail.com>
2024-02-28 22:16:14 +0200

    Add needed annotation + testing lines

--- src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -4,11 +4,13 @@ import java.util.Optional;
 import mate.academy.exception.AuthenticationException;
 import mate.academy.exception.RegistrationException;
 import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
 import mate.academy.model.User;
 import mate.academy.service.AuthenticationService;
 import mate.academy.service.UserService;
 import mate.academy.util.HashUtil;
 
+@Service
 public class AuthenticationServiceImpl implements AuthenticationService {
     @Inject
     private UserService userService;
@@ -21,6 +23,9 @@ public class AuthenticationServiceImpl implements AuthenticationService {
             user.setEmail(email);
             user.setPassword(password);
             userService.add(user);
+            //only for testing, I know it shouldn't be here
+            System.out.println("User added to DB:");
+            System.out.println(userService.get(user.getId()));
         } else {
             throw new RegistrationException("Can't register new user: user with email "
                     + email + " already exists");
@@ -31,13 +36,15 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     public User login(String email, String password) throws AuthenticationException {
         Optional<User> userFromDbOptional = userService.findByEmail(email);
         if (userFromDbOptional.isEmpty()) {
-            throw new AuthenticationException("Can't authenticate user");
+            throw new AuthenticationException("Can't authenticate user: user with email "
+                    + email + " doesn't exist");
         }
         User user = userFromDbOptional.get();
         String hashedPassword = HashUtil.hashPassword(password, user.getSalt());
         if (user.getPassword().equals(hashedPassword)) {
             return user;
         }
-        throw new AuthenticationException("Can't authenticate user");
+        throw new AuthenticationException("Can't authenticate user: "
+                + "invalid password. Try again...");
     }
 }
