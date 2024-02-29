9ab36eb17bfb74b59f9578c1d3841e77c5ae65d9
Adam Mudrak <113807446+AdamMudrak@users.noreply.github.com>
2024-02-29 13:28:47 +0200

    Update src/main/java/mate/academy/service/AuthenticationService.java
    Co-authored-by: Anton Kozhukhivskiy <a.kozhukhovsky@gmail.com>

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -5,7 +5,6 @@ import mate.academy.exception.RegistrationException;
 import mate.academy.model.User;
 
 public interface AuthenticationService {
-
     void register(String email, String password) throws RegistrationException;
 
     User login(String email, String password) throws AuthenticationException;
