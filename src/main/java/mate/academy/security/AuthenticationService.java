be157faa7d9e2402ae568f74dbc7ecc81ddaa020
makaroshyna <katya23225@gmail.com>
2024-04-05 17:41:40 +0300

    Change imports.

--- src/main/java/mate/academy/security/AuthenticationService.java
+++ src/main/java/mate/academy/security/AuthenticationService.java
@@ -1,6 +1,6 @@
 package mate.academy.security;
 
-import mate.academy.exception.AuthenticationException;
+import javax.naming.AuthenticationException;
 import mate.academy.exception.RegistrationException;
 import mate.academy.model.User;
 
