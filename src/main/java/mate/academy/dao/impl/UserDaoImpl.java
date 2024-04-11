475931f9bc758b2e05681f640a9bc37dbf079e74
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-04-11 14:48:46 +0300

    created UserDaoImpl

--- src/main/java/mate/academy/dao/impl/UserDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/UserDaoImpl.java
@@ -1,2 +1,18 @@
-package mate.academy.dao.impl;public class UserDaoImpl {
+package mate.academy.dao.impl;
+
+import mate.academy.dao.UserDao;
+import mate.academy.model.User;
+
+import java.util.Optional;
+
+public class UserDaoImpl implements UserDao {
+    @Override
+    public User add(User user) {
+        return null;
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return Optional.empty();
+    }
 }
