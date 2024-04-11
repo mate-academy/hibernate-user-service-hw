80023c343b4372400984da61fd399ba720a8a4a1
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-04-11 14:43:58 +0300

    implemented User class

---
+++ src/main/java/mate/academy/model/User.java
@@ -0,0 +1,17 @@
+package mate.academy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String email;
+    String password;
+}
