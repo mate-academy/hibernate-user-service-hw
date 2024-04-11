601250b8cf161fe4643d9f891294de27ccf4de21
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-04-11 16:44:59 +0300

    created AuthenticationService and AuthenticationServiceImpl

---
+++ src/main/java/mate/academy/auth/AuthenticationServiceImpl.java
@@ -0,0 +1,15 @@
+package mate.academy.auth;
+
+import mate.academy.model.User;
+
+public class AuthenticationServiceImpl implements AuthenticationService{
+    @Override
+    public User login(String email, String password) {
+        return null;
+    }
+
+    @Override
+    public User register(String email, String password) {
+        return null;
+    }
+}
