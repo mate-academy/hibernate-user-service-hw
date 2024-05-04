de9b54493a303a9f5626d5f51175173652879813
Oleksand Kolisnyk <kolisnykol@gmail.com>
2024-05-04 12:23:13 +0300

    added  public static final int SALT_LENGTH = 16;

--- src/main/java/mate/academy/util/HashUtil.java
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -6,13 +6,14 @@ import java.security.SecureRandom;
 
 public class HashUtil {
     public static final String CRYPTO_ALGORITHM = "SHA-512";
+    public static final int SALT_LENGTH = 16;
 
     private HashUtil() {
     }
 
     public static byte[] getSalt() {
         SecureRandom secureRandom = new SecureRandom();
-        byte[] salt = new byte[16];
+        byte[] salt = new byte[SALT_LENGTH];
         secureRandom.nextBytes(salt);
         return salt;
     }
