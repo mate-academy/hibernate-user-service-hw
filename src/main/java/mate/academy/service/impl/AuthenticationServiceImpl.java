ff3d17cab3f8d3980447b144da2915552691498b
Dima <dmmhnk23@gmail.com>
2024-05-17 11:18:37 +0200

    finished task

--- src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -1,5 +1,6 @@
 package mate.academy.service.impl;
 
+import java.util.Optional;
 import mate.academy.exception.AuthenticationException;
 import mate.academy.exception.RegistrationException;
 import mate.academy.lib.Inject;
@@ -7,7 +8,6 @@ import mate.academy.lib.Service;
 import mate.academy.model.User;
 import mate.academy.service.AuthenticationService;
 import mate.academy.service.UserService;
-import java.util.Optional;
 import mate.academy.util.HashUtil;
 
 @Service
