fac75b5577f906e0f4fc87690b97ffad401cdb0f
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-06-20 12:57:02 +0300

    implemented UserDao, UserService & AuthenticationService

---
+++ src/main/java/mate/academy/exception/RegistrationException.java
@@ -0,0 +1,11 @@
+package mate.academy.exception;
+
+public class RegistrationException extends Exception {
+    public RegistrationException(String message) {
+        super(message);
+    }
+
+    public RegistrationException(String message, Exception e) {
+        super(message, e);
+    }
+}
