59f1e5d54a7a1fd14175752340c8dc5b1d158ce9
Maksim <maksim.sokur90@gmail.com>
2024-06-23 18:04:15 +0300

    hibernate-user-service-hw-sokur-v1 1. Create models:    User 2. Create DAO    User DAO 3. Create service    UserService    AuthenticationService 4. Create checked RegistrationException and AuthenticationException 5. Use salt in password hiding 6. In method main create instance and inject all methods

---
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -0,0 +1,33 @@
+package mate.academy.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class HashUtil {
+    private static final String CRYPTO_ALGORITHM = "SHA-512";
+
+    private HashUtil() {
+    }
+
+    public static byte[] getSalt() {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        return salt;
+    }
+
+    public static String hashPassword(String password, byte[] salt) {
+        StringBuilder hashedPassword = new StringBuilder();
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance(CRYPTO_ALGORITHM);
+            byte[] hash = messageDigest.digest(password.getBytes());
+            for (byte b : hash) {
+                hashedPassword.append(String.format("%02x", b));
+            }
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException("Could not hash password using SHA-512.", e);
+        }
+        return hashedPassword.toString();
+    }
+}
