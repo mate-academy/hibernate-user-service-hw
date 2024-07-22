70e65632d9834bb7f1a4959f98902df18b79effc
Milosz <szwajkowski.milosz@gmail.com>
2024-07-22 15:16:49 +0200

    Fixed styling

--- src/main/java/mate/academy/util/HashUtil.java
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -7,7 +7,8 @@ import java.security.SecureRandom;
 public class HashUtil {
     private static final String CRYPTO_ALGORITHM = "SHA-512";
 
-    private HashUtil() {}
+    private HashUtil() {
+    }
 
     public static byte[] getSalt() {
         SecureRandom secureRandom = new SecureRandom();
@@ -26,7 +27,8 @@ public class HashUtil {
                 hashedPassword.append(String.format("%02x", b));
             }
         } catch (NoSuchAlgorithmException e) {
-            throw new IllegalStateException("Could not create hash using algorithm: " + CRYPTO_ALGORITHM, e);
+            throw new IllegalStateException("Could not create hash using algorithm: "
+                    + CRYPTO_ALGORITHM, e);
         }
         return hashedPassword.toString();
     }
