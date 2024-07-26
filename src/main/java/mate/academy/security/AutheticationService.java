06ce501812290380f678d70ed072f24967e4b0e8
BelitskyiAlexandr <vilisapett@gmail.com>
2024-07-26 23:10:35 +0300

    comments are deleted

--- src/main/java/mate/academy/security/AutheticationService.java
+++ src/main/java/mate/academy/security/AutheticationService.java
@@ -7,11 +7,5 @@ import mate.academy.model.User;
 public interface AutheticationService {
     User login(String email, String password) throws AuthenticationException;
 
-    /**
-     * We should register a new user. The new user entity will contain the email and password
-     * @param email - user email. should be unique for each user
-     * @param password - user password
-     * @return new user instance
-     */
     User register(String email, String password) throws RegistrationException;
 }
