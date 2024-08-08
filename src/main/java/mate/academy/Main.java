9c019c6088ae55af962c3075028be550ba932e6d
Mykhailo Kazarian <misha.kazaryan2@gmail.com>
2024-08-08 16:48:59 +0300

    Hibernate user service task solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -2,16 +2,26 @@ package mate.academy;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Injector;
 import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
 import mate.academy.model.MovieSession;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
 import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
+import mate.academy.service.impl.AuthenticationServiceImpl;
 
 public class Main {
+
+    private static final Injector INJECTOR = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
-        MovieService movieService = null;
+        MovieService movieService =(MovieService) INJECTOR.getInstance(MovieService.class);
 
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
@@ -27,7 +37,7 @@ public class Main {
         secondCinemaHall.setCapacity(200);
         secondCinemaHall.setDescription("second hall with capacity 200");
 
-        CinemaHallService cinemaHallService = null;
+        CinemaHallService cinemaHallService = (CinemaHallService) INJECTOR.getInstance(CinemaHallService.class);
         cinemaHallService.add(firstCinemaHall);
         cinemaHallService.add(secondCinemaHall);
 
@@ -44,12 +54,30 @@ public class Main {
         yesterdayMovieSession.setMovie(fastAndFurious);
         yesterdayMovieSession.setShowTime(LocalDateTime.now().minusDays(1L));
 
-        MovieSessionService movieSessionService = null;
+        MovieSessionService movieSessionService = (MovieSessionService) INJECTOR.getInstance(MovieSessionService.class);
         movieSessionService.add(tomorrowMovieSession);
         movieSessionService.add(yesterdayMovieSession);
 
         System.out.println(movieSessionService.get(yesterdayMovieSession.getId()));
         System.out.println(movieSessionService.findAvailableSessions(
                         fastAndFurious.getId(), LocalDate.now()));
+
+        AuthenticationService authenticationService = (AuthenticationService) INJECTOR.getInstance(AuthenticationService.class);
+
+        try {
+            System.out.println(authenticationService.register("Mate@gamil.com","12345qwert"));
+            System.out.println(authenticationService.register("Academy@gamil.com","qwert"));
+            System.out.println(authenticationService.register("LOLIPOP123f@gamil.com","098765dfde"));
+        } catch (RegistrationException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            System.out.println(authenticationService.login("Mate@gamil.com","12345qwert"));
+        } catch (AuthenticationException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
---
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -0,0 +1,43 @@
+package mate.academy.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class HashUtil {
+    private static final String ALGORITHM = "SHA-512";
+
+    private HashUtil(){
+
+    }
+    public static byte[] getSalt(){
+        try {
+            SecureRandom sr = SecureRandom.getInstanceStrong();
+            byte[] salt = new byte[16];
+            sr.nextBytes(salt);
+            return salt;
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Cannot generate salt", e);
+        }
+    }
+
+    public static String getHashOfPassword(String password, byte[] salt){
+        StringBuilder hashPassword = new StringBuilder();
+
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance(ALGORITHM);
+            messageDigest.update(salt);
+
+            byte[] digest = messageDigest.digest(password.getBytes());
+
+            for(byte b : digest){
+                hashPassword.append(String.format("%02x",b));
+            }
+
+            return hashPassword.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Can't get hash of password with algorithm: " + ALGORITHM, e);
+        }
+    }
+}
