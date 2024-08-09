f97620292b3c2f316c096cb21ca8855fdc2957ba
Illya Pantazi <illios.piv@gmail.com>
2024-08-09 18:10:09 +0200

    The solution of the `hibernate-user-service-hw`: Created interfaces: `UserDao`, `AuthenticationService`, `UserService`. Created a file `hibernate.cfg.xml` and edited `pom.xml`. Created classes: `User`, `UserDaoImpl`, `AuthenticationServiceImpl`, `UserServiceImpl`, `HashUtil`, `AuthenticationException`, `RegistrationException`. Edited classes: `Main`, `MovieSession`.

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -5,7 +5,7 @@ import mate.academy.exception.RegistrationException;
 import mate.academy.model.User;
 
 public interface AuthenticationService {
-    public void register(String email, String password) throws RegistrationException;
+    void register(String email, String password) throws RegistrationException;
 
-    public User login(String email, String password) throws AuthenticationException;
+    User login(String email, String password) throws AuthenticationException;
 }
