8d0a4d3f22b98a16bae88994b061819a3ff4817e
Andrey Chertykovtsev <chertiav@gmail.com>
2024-09-09 10:28:39 +0200

    Created user service: - added user authentication and registration with hashing

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,55 +1,49 @@
 package mate.academy;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import mate.academy.model.CinemaHall;
-import mate.academy.model.Movie;
-import mate.academy.model.MovieSession;
-import mate.academy.service.CinemaHallService;
-import mate.academy.service.MovieService;
-import mate.academy.service.MovieSessionService;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Injector;
+import mate.academy.model.User;
+import mate.academy.security.AuthenticationService;
 
 public class Main {
+    private static final Injector INJECTOR = Injector.getInstance("mate.academy");
+    private static final String TEST_EMAIL = "test@gmail.com";
+    private static final String TEST_PASSWORD = "test";
+    private static final String WRONG_PASSWORD = "wrong";
+
     public static void main(String[] args) {
-        MovieService movieService = null;
+        AuthenticationService authenticationService = (AuthenticationService)
+                INJECTOR.getInstance(AuthenticationService.class);
 
-        Movie fastAndFurious = new Movie("Fast and Furious");
-        fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
-        movieService.add(fastAndFurious);
-        System.out.println(movieService.get(fastAndFurious.getId()));
-        movieService.getAll().forEach(System.out::println);
+        // case of registration with a non-existing email
+        try {
+            User registeruser = authenticationService.register(TEST_EMAIL, TEST_PASSWORD);
+            System.out.println(registeruser);
+        } catch (RegistrationException e) {
+            throw new RuntimeException(e.getMessage());
+        }
 
-        CinemaHall firstCinemaHall = new CinemaHall();
-        firstCinemaHall.setCapacity(100);
-        firstCinemaHall.setDescription("first hall with capacity 100");
+        // case of logging in with the correct login and password
+        try {
+            User userLogin = authenticationService.login(TEST_EMAIL, TEST_PASSWORD);
+            System.out.println(userLogin);
+        } catch (AuthenticationException e) {
+            throw new RuntimeException(e.getMessage());
+        }
 
-        CinemaHall secondCinemaHall = new CinemaHall();
-        secondCinemaHall.setCapacity(200);
-        secondCinemaHall.setDescription("second hall with capacity 200");
+        // case of registration with existing email
+        try {
+            authenticationService.register(TEST_EMAIL, TEST_PASSWORD);
+        } catch (RegistrationException e) {
+            System.out.println(e.getMessage());
+        }
 
-        CinemaHallService cinemaHallService = null;
-        cinemaHallService.add(firstCinemaHall);
-        cinemaHallService.add(secondCinemaHall);
-
-        System.out.println(cinemaHallService.getAll());
-        System.out.println(cinemaHallService.get(firstCinemaHall.getId()));
-
-        MovieSession tomorrowMovieSession = new MovieSession();
-        tomorrowMovieSession.setCinemaHall(firstCinemaHall);
-        tomorrowMovieSession.setMovie(fastAndFurious);
-        tomorrowMovieSession.setShowTime(LocalDateTime.now().plusDays(1L));
-
-        MovieSession yesterdayMovieSession = new MovieSession();
-        yesterdayMovieSession.setCinemaHall(firstCinemaHall);
-        yesterdayMovieSession.setMovie(fastAndFurious);
-        yesterdayMovieSession.setShowTime(LocalDateTime.now().minusDays(1L));
-
-        MovieSessionService movieSessionService = null;
-        movieSessionService.add(tomorrowMovieSession);
-        movieSessionService.add(yesterdayMovieSession);
-
-        System.out.println(movieSessionService.get(yesterdayMovieSession.getId()));
-        System.out.println(movieSessionService.findAvailableSessions(
-                        fastAndFurious.getId(), LocalDate.now()));
+        //case of logging in with the wrong password
+        try {
+            authenticationService.login(TEST_EMAIL, WRONG_PASSWORD);
+        } catch (AuthenticationException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
