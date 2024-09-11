1e0266d0fb70f8935bb76f434edaf579873d962e
Oleh B <oleg.busygin@gmail.com>
2024-09-11 14:53:31 +0300

    Hibernate user service task done

---
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,46 @@
+package mate.academy.service.impl;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> userFromDbOptional = userService.findByEmail(email);
+        if (userFromDbOptional.isEmpty()) {
+            throw new AuthenticationException("email or password is not correct. email: "
+                    + email + ", password: " + password);
+        }
+        User user = userFromDbOptional.get();
+        if (!user.getPassword().equals(password)) {
+            throw new AuthenticationException("email or password is not correct. email: "
+                    + email + ", password: " + password);
+        }
+        return user;
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        Optional<User> userFromDbOptional = userService.findByEmail(email);
+        if (userFromDbOptional.isPresent()) {
+            throw new RegistrationException("User with email: " + email + "already exists");
+        }
+        if (password.isEmpty()) {
+            throw new RegistrationException("Password cannot be empty");
+        }
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        return userService.add(user);
+    }
+}
