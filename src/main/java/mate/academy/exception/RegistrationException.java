48cf4c4e7209943d8ae754480843ef5d7a1133cf
OD <sdenisenko055@gmail.com>
2024-09-12 16:17:13 +0300

    result

---
+++ src/main/java/mate/academy/exception/RegistrationException.java
@@ -0,0 +1,10 @@
+package mate.academy.exception;
+
+public class RegistrationException extends Exception {
+    public RegistrationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public RegistrationException(String message) {
+    }
+}
