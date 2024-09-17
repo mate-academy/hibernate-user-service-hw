1a19094724e0da96b09ce6e108b23065627e06d8
Margaryta <marharytayarmoliuk@gmail.com>
2024-09-17 21:19:25 +0300

    All problems fixed

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -15,7 +15,7 @@ public class User {
     private Long id;
     private String email;
     private String password;
-    private byte [] salt;
+    private byte[] salt;
 
     public User() {
     }
