d541a07b56bc9f9e9df08cfd6f6c81d41f7699fa
Orest Sorokowey <osorokowey@gmail.com>
2024-09-27 12:58:20 +0300

    changed the user class acccording to the naming convention

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
