716383b3d6f57c155a0736069421724ffe17a2c4
trokhim03 <trokhimchuk.003@gmail.com>
2024-10-23 11:29:07 +0300

    made some changes

--- src/main/java/mate/academy/security/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/security/AuthenticationServiceImpl.java
@@ -28,13 +28,12 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public User login(String email, String password) throws AuthenticationException {
         Optional<User> optionalUser = userService.findByEmail(email);
-        if (optionalUser.isEmpty()) {
-            throw new AuthenticationException("Can't authentication user");
-        }
-        User user = optionalUser.get();
-        String hashedPassword = HashUtil.hashPassword(password, user.getSalt());
-        if (user.getPassword().equals(hashedPassword)) {
-            return user;
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            String hashedPassword = HashUtil.hashPassword(password, user.getSalt());
+            if (user.getPassword().equals(hashedPassword)) {
+                return user;
+            }
         }
         throw new AuthenticationException("Can't authentication user");
     }
