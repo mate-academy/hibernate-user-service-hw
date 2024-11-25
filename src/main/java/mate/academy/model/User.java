dc903de4b46f8f092e4a36b541219c83a8ba3b04
kankeii <vyrvyjo@gmail.com>
2024-11-25 15:53:13 +0200

    remake some errors

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -54,8 +54,6 @@ public class User {
                 + "id=" + id
                 + ", email='" + email
                 + '\''
-                + ", password='" + password
-                + '\''
                 + '}';
     }
 }
