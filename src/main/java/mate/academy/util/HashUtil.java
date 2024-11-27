76c94174ae744bfc3bb085181e9e59dd518312d8
Igor Neminuschii <igorgorslav@gmail.com>
2024-11-27 11:26:10 +0200

    Added constants

--- src/main/java/mate/academy/util/HashUtil.java
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -5,14 +5,16 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
 public class HashUtil {
+    private static final int SALT_AMOUNT = 16;
     private static final String CRYPTO_ALGORITHM = "SHA-512";
+    private static final String BYTE_FORMAT = "%02x";
 
     private HashUtil() {
     }
 
     public static byte[] getSalt() {
         SecureRandom secureRandom = new SecureRandom();
-        byte[] salt = new byte[16];
+        byte[] salt = new byte[SALT_AMOUNT];
         secureRandom.nextBytes(salt);
         return salt;
     }
@@ -24,7 +26,7 @@ public class HashUtil {
             messageDigest.update(salt);
             byte[] digest = messageDigest.digest(password.getBytes());
             for (byte b : digest) {
-                hashedPassword.append(String.format("%02x", b));
+                hashedPassword.append(String.format(BYTE_FORMAT, b));
             }
         } catch (NoSuchAlgorithmException e) {
             throw new IllegalStateException(
