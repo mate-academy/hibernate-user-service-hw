fbca3a87046efd84ad66fe6fcc3eb43e6b41fea4
Sergii Konovalov <sergii2konovalov@gmail.com>
2025-02-19 11:28:21 +0200

    hw-solution

---
+++ src/main/java/mate/academy/security/AuthenticationServiceImpl.java
@@ -0,0 +1,44 @@
+package mate.academy.security;
+
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public void register(String email, String password) {
+        if (email.isBlank() || password.isBlank()) {
+            throw new RegistrationException("Email and password cannot be empty");
+        }
+
+        userService.findByEmail(email).ifPresent(u -> {
+            throw new RegistrationException("User with this email already exists");
+        });
+
+        User user = new User();
+        user.setSalt(HashUtil.getSalt());
+        user.setEmail(email);
+        user.setPassword(HashUtil.hashPassword(password, user.getSalt()));
+        userService.add(user);
+    }
+
+    @Override
+    public User login(String login, String password) {
+        User user = userService.findByEmail(login)
+                .orElseThrow(() -> new AuthenticationException("Cannot authenticate user"));
+
+        String hashPassword = HashUtil.hashPassword(password, user.getSalt());
+        if (!user.getPassword().equals(hashPassword)) {
+            throw new AuthenticationException("Cannot authenticate user");
+        }
+        return user;
+    }
+}
