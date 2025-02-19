d6cbd25e23b990913dc0ab9d8d3a1b77746b102c
Pavlo Mykhailyk <pmwonderchrono@gmail.com>
2025-02-19 13:33:05 +0100

    2

--- src/main/java/mate/academy/util/HashUtil.java
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -7,9 +7,10 @@ import java.security.SecureRandom;
 public class HashUtil {
     private static final String CRYPTO_ALGORITHM = "SHA-512";
 
-    private HashUtil(){
+    private HashUtil() {
     }
-    public static byte[] getSalt(){
+
+    public static byte[] getSalt() {
         SecureRandom secureRandom = new SecureRandom();
         byte[] salt = new byte[16];
         secureRandom.nextBytes(salt);
@@ -17,19 +18,19 @@ public class HashUtil {
 
     }
 
-    public static String HashPassword(String password, byte [] salt){
+    public static String hashPassword(String password, byte[] salt) {
         StringBuilder hashedPassword = new StringBuilder();
         try {
             MessageDigest messageDigest = MessageDigest.getInstance(CRYPTO_ALGORITHM);
             messageDigest.update(salt);
-            byte [] digest = messageDigest.digest(password.getBytes());
-            for (byte b : digest){
+            byte[] digest = messageDigest.digest(password.getBytes());
+            for (byte b : digest) {
                 hashedPassword.append(String.format("%02x", b));
             }
         } catch (NoSuchAlgorithmException e) {
-            throw new IllegalStateException("IllegalStateException SHA-512",e);
+            throw new IllegalStateException("IllegalStateException SHA-512", e);
         }
 
         return hashedPassword.toString();
-}
+    }
 }
