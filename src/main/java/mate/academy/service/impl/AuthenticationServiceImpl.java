885361f536f51a53fd775bf2a6dbc5a7f25300f3
Pavlo Mykhailyk <pmwonderchrono@gmail.com>
2025-02-19 14:09:03 +0100

    2

--- src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -1,14 +1,28 @@
 package mate.academy.service.impl;
 
+import java.util.Optional;
 import mate.academy.exception.AuthenticationException;
 import mate.academy.exception.RegistrationException;
 import mate.academy.model.User;
 import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
+    private UserService userService;
+
     @Override
     public User login(String email, String password) throws AuthenticationException {
-        return null;
+        Optional<User> UserFromDB = userService.findByEmail(email);
+        if (UserFromDB.isEmpty()){
+            throw new AuthenticationException("Can t authenticate User");
+        }
+        User user = UserFromDB.get();
+        String hashedPassword = HashUtil.hashPassword(password, user.getSalt());
+        if (user.getPassword().equals(hashedPassword)){
+            return user;
+        }
+        throw new AuthenticationException("Can t authenticate User");
     }
 
     @Override
