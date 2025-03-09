f3663ec9f23e82334fd2082b1772efd91c87c67a
Dmytro Tkachuk <dmitrotkacuk071@gmail.com>
2025-03-09 21:35:59 +0100

    Created models: User Created DAO: UserDAO Created services: UserService: Created own tested RegistrationException and AuthenticationException for use in register() and login() methods respectively Used salt and password hashing. In mate/academy/Main.main() method created an AuthenticationService instance using the injector and tested all methods from it.

---
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,52 @@
+package mate.academy.service.impl;
+
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public void register(String email, String password) throws RegistrationException {
+        if (email.isBlank() || password.isBlank()) {
+            try {
+                throw new RegistrationException("Email and password cannot be empty");
+            } catch (RegistrationException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        if (userService.findByEmail(email).isPresent()) {
+            try {
+                throw new RegistrationException("Email and password cannot be empty");
+            } catch (RegistrationException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        userService.add(user);
+
+    }
+
+    @Override
+    public User login(String login, String password) throws AuthenticationException {
+        User user = userService.findByEmail(login)
+                .orElseThrow(() -> new AuthenticationException("Cannot authenticate user"));
+
+        String hashPassword = HashUtil.hashPassword(password, user.getSalt());
+        if (!user.getPassword().equals(hashPassword)) {
+            throw new AuthenticationException("Cannot authenticate user");
+        }
+        return user;
+    }
+}
