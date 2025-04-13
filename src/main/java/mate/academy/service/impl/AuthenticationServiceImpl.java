1b60b47e12625f07ec405e7643b6f435bef7697b
Takeit <larsubivec@gmail.com>
2025-04-13 15:45:10 +0400

    implemented all required dao, services, models and custom exceptions

---
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,42 @@
+package mate.academy.service.impl;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> optionalUserFromDb = userService.findByEmail(email);
+        if (optionalUserFromDb.isPresent()) {
+            User user = optionalUserFromDb.get();
+            if (user.getPassword().equals(HashUtil.hashPassword(password, user.getSalt()))) {
+                return user;
+            }
+        }
+        throw new AuthenticationException("Can`t authenticate User with email " + email);
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        Optional<User> optionalUserFromDb = userService.findByEmail(email);
+        if (optionalUserFromDb.isEmpty()) {
+            User user = new User();
+            user.setEmail(email);
+            user.setPassword(password);
+            userService.add(user);
+            return user;
+        }
+        throw new RegistrationException("Can`t register User with email " + email);
+    }
+}
